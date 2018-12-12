67f9116dbe9fdbe47e8c11ccd61620b251630935
Heiko Burkhardt <heiko@slock.it>
2018-12-12 11:13:00 +0100

    cors

--- packages/ew-utils-testbackend/index.js
+++ packages/ew-utils-testbackend/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+  }
+
 
 
 const producingAssets = []
@@ -9,26 +17,26 @@ const consumingAssets = []
 app.use(bodyParser.json())
 
 
-app.get('/ProducingAsset/:id', (req, res) => {
+app.get('/ProducingAsset/:id', cors(corsOptions), (req, res) => {
   console.log(`GET - ProducingAsset ${req.params.id}`)
   res.send(producingAssets[req.params.id])
 })
 
-app.put('/ProducingAsset/:id', function (req, res) {
+app.put('/ProducingAsset/:id', cors(corsOptions), function (req, res) {
   console.log(`PUT - ProducingAsset ${req.params.id}`)
   producingAssets[req.params.id] = req.body
   res.send("success")
 })
 
 
-app.get('/ConsumingAsset/:id', (req, res) => {
+app.get('/ConsumingAsset/:id', cors(corsOptions), (req, res) => {
   console.log(`GET - ConsumingAsset ${req.params.id}`)
   res.send(consumingAssets[req.params.id])
 })
 
 
 
-app.put('/ConsumingAsset/:id', function (req, res) {
+app.put('/ConsumingAsset/:id', cors(corsOptions), (req, res) => {
   console.log(`PUT - ConsumingAsset ${req.params.id}`)
   consumingAssets[req.params.id] = req.body
   res.send("success")
