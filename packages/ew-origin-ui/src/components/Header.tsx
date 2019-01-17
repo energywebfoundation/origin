cb7ee9f716ae49f488a22b18badf3cffcd30717a
Heiko Burkhardt <heiko@slock.it>
2019-01-17 13:10:52 +0100

    certs

--- packages/ew-origin-ui/src/components/Header.tsx
+++ packages/ew-origin-ui/src/components/Header.tsx
@@ -50,8 +50,8 @@ export class Header extends React.Component<HeaderProps, {}> {
         <NavLink to='/assets' activeClassName='active'><img src={logo as any} /></NavLink>
         <Nav className="NavMenu">
           <li><NavLink to={'/' + this.props.baseUrl + '/assets'} activeClassName='active'>Assets</NavLink></li>
-          {/* <li><NavLink to={'/' + this.props.baseUrl + '/certificates'} activeClassName='active'>Certificates</NavLink></li>
-          <li><NavLink to={'/' + this.props.baseUrl + '/demands'} activeClassName='active'>Demands</NavLink></li>
+          <li><NavLink to={'/' + this.props.baseUrl + '/certificates'} activeClassName='active'>Certificates</NavLink></li>
+          {/* <li><NavLink to={'/' + this.props.baseUrl + '/demands'} activeClassName='active'>Demands</NavLink></li>
           <li><NavLink to={'/' + this.props.baseUrl + '/admin'} activeClassName='active'>Admin</NavLink></li> */}
         </Nav>
         <div className="ViewProfile">
