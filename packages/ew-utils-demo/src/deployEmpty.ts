f1d20cdd58cf5067616e49866dcf2ee6966b7d39
Chirag Mahaveer Parmar <chiragmahaveerparmar@Chirags-MacBook-Pro.local>
2019-01-30 00:47:42 +0100

    FIX: json object storage

--- packages/ew-utils-demo/src/deployEmpty.ts
+++ packages/ew-utils-demo/src/deployEmpty.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 import { Web3Type } from './types/web3';
-import { migrateUserRegistryContracts, UserContractLookup} from 'ew-user-registry-contracts';
-import { migrateAssetRegistryContracts, AssetContractLookup} from 'ew-asset-registry-contracts';
-import { migrateMarketRegistryContracts, MarketContractLookup} from 'ew-market-contracts';
+import { migrateUserRegistryContracts } from 'ew-user-registry-contracts';
+import { migrateAssetRegistryContracts } from 'ew-asset-registry-contracts';
+import { migrateMarketRegistryContracts } from 'ew-market-contracts';
 //import { migrateCertificateRegistryContracts, migrateEnergyBundleContracts} from 'ew-origin-contracts'
 
 
@@ -24,23 +24,27 @@ export const deployEmptyContracts = async() => {
   //deploy user, asset and market contracts and store instances of lookup contracts
   const userContracts = await migrateUserRegistryContracts((web3 as any))
   const userContractLookup = userContracts[process.cwd() + '/node_modules/ew-user-registry-contracts/dist/contracts/UserContractLookup.json']
-  console.log("User Contract Deployed")
+  console.log("User Contract Deployed: " + userContractLookup)
 
   const assetContracts = await migrateAssetRegistryContracts((web3 as any), userContractLookup)
   const assetContractLookup = assetContracts[process.cwd() + "/node_modules/ew-asset-registry-contracts/dist/contracts/AssetContractLookup.json"]
-  console.log("Asset Contract Deployed")
+  console.log("Asset Contract Deployed: " + assetContractLookup)
 
   const marketContracts = await migrateMarketRegistryContracts((web3 as any), assetContractLookup)
-  const marketContractLookup = assetContracts[process.cwd() + "/node_modules/ew-market-contracts/dist/contracts/MarketContractLookup.json"]
-  console.log("Market Contract Deployed")
+  const marketContractLookup = marketContracts[process.cwd() + "/node_modules/ew-market-contracts/dist/contracts/MarketContractLookup.json"]
+  console.log("Market Contract Deployed: " + marketContractLookup)
 
   //initialise all contracts
   //migrateContracts already intializes the contracts
 
-  let deployResult = {}
-  deployResult["userContractLookup"] = userContractLookup
-  deployResult["assetContractLookup"] = assetContractLookup
-  deployResult["marketContractLookup"] = marketContractLookup
+  let deployResult = {} as any
+  deployResult.userContractLookup = userContractLookup
+  deployResult.assetContractLookup = assetContractLookup
+  deployResult.marketContractLookup = marketContractLookup
+
+  const writeJsonFile = require('write-json-file')
+  await writeJsonFile('contractConfig.json', deployResult)
+
 
 }
 
