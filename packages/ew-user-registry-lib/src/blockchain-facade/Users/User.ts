0b36324c8ee308d8486e1b81c91c9835b469187a
Martin Kuechler <martin@slock.it>
2019-01-31 09:12:13 +0000

    Merge branch 'blockchain-props-factory' into 'develop'
    Blockchain props factory
    
    See merge request ewf/origin/user-registry/ew-user-registry-lib!6

--- packages/ew-user-registry-lib/src/blockchain-facade/Users/User.ts
+++ packages/ew-user-registry-lib/src/blockchain-facade/Users/User.ts
@@ -42,8 +42,10 @@ export class User extends BlockchainDataModelEntity implements UserProperties {
                 from: configuration.blockchainProperties.activeUser.address,
                 privateKey: configuration.blockchainProperties.activeUser.privateKey,
             });
-
-        configuration.logger.info(`User ${userProperties.id} synced`);
+        if (configuration.logger) {
+            configuration.logger.info(`User ${userProperties.id} synced`);
+        }
+        
         return (new User(userProperties.id, configuration)).sync();
     }
 
@@ -65,7 +67,10 @@ export class User extends BlockchainDataModelEntity implements UserProperties {
             this.organization = userData._organization;
             this.roles = parseInt(userData._roles, 10);
             this.active = userData._active;
-            this.configuration.logger.verbose(`User ${this.id} synced`);
+            if (this.configuration.logger) {
+                this.configuration.logger.verbose(`User ${this.id} synced`);
+            }
+            
 
         }
         return this;
