a51f6986dcfb76dc3c42b6a74cafed5e449dfc28
Martin Kuechler <martin@slock.it>
2019-02-12 09:49:06 +0000

    Merge branch '1-clean-up' into 'develop'
    Resolve "Clean up"
    
    See merge request ewf/origin/user-registry/ew-user-registry-lib!7

--- packages/ew-user-registry-lib/src/test/UserLogicFacade.ts
+++ packages/ew-user-registry-lib/src/test/UserLogicFacade.ts
@@ -1,6 +1,6 @@
 // Copyright 2018 Energy Web Foundation
 // This file is part of the Origin Application brought to you by the Energy Web Foundation,
-// a global non-profit organization focused on accelerating blockchain technology across the energy sector, 
+// a global non-profit organization focused on accelerating blockchain technology across the energy sector,
 // incorporated in Zug, Switzerland.
 //
 // The Origin Application is free software: you can redistribute it and/or modify
@@ -12,7 +12,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
 //
-// @authors: slock.it GmbH, Martin Kuechler, martin.kuechler@slock.it
+// @authors: slock.it GmbH; Heiko Burkhardt, heiko.burkhardt@slock.it; Martin Kuechler, martin.kuchler@slock.it
 
 import { assert } from 'chai';
 import * as fs from 'fs';
@@ -22,7 +22,7 @@ import { UserLogic, UserContractLookup, migrateUserRegistryContracts, UserContra
 import { UserProperties, UserPropertiesOffChain, User } from '../blockchain-facade/Users/User';
 import { Configuration } from 'ew-utils-general-lib';
 import { logger } from '../blockchain-facade/Logger';
-import Web3 = require('web3');
+import Web3 from 'web3';
 
 describe('UserLogic Facade', () => {
     const configFile = JSON.parse(fs.readFileSync(process.cwd() + '/connection-config.json', 'utf8'));
@@ -48,18 +48,18 @@ describe('UserLogic Facade', () => {
     it('should deploy the contracts', async () => {
 
         const contracts = await migrateUserRegistryContracts((web3 as any), privateKeyDeployment) as any;
-    
+
         userContractLookup = new UserContractLookup((web3 as any), contracts.UserContractLookup);
         userRegistry = new UserLogic((web3 as any), await userContractLookup.userRegistry());
 
-  
+
 
         // const bytecodeUserContractLookup = await web3.eth.getCode(contracts.UserContractLookup);
         // assert.isTrue(bytecodeUserContractLookup.length > 0);
         // assert.equal(bytecodeUserContractLookup, '0x' + UserContractLookupJSON.bytecode);
 
 
-     
+
 
     });
 
