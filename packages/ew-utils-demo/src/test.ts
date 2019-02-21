b5afa43f4e3158ea4238a92a5e9525fa56c25cd7
Chirag Mahaveer Parmar <chirag.parmar@slock.it>
2019-02-21 23:35:18 +0100

    config changes

--- packages/ew-utils-demo/src/test.ts
+++ packages/ew-utils-demo/src/test.ts
@@ -7,7 +7,7 @@ async function main() {
     console.log(contractConfig)
 
     const demoFile = fs.readFileSync("./config/demo-config-2.json", 'utf8').toString()
-    await marketDemo(demoFile)
+    await marketDemo()
 }
 
 main()
