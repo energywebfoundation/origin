e00dbfe4432aead3dce2e03ea47af6b97c1da69f
Chirag Mahaveer Parmar <chirag.parmar@slock.it>
2019-02-21 12:32:26 +0100

    schema export fix

--- packages/ew-market-matcher/src/exports.ts
+++ packages/ew-market-matcher/src/exports.ts
@@ -4,13 +4,9 @@ import { SimpleMatcher } from './matcher/SimpleMatcher';
 import { ConfigurableReferenceMatcher } from './matcher/ConfigurableReferenceMatcher';
 import { Controller } from './controller/Controller';
 import { SimulationModeController } from './controller/SimulationModeController';
-import * as Winston from 'winston';
-import * as ConfSchema from '../schemas/conf.schema.json';
-import * as RuleSchema from '../schemas/rule.schema.json';
 import { BlockchainModeController } from './controller/BlockchainModeController';
 import { createBlockchainConf } from './controller/BlockchainConnection';
 
 export {
-    SchemaDefs, Matcher, SimpleMatcher, ConfigurableReferenceMatcher, Controller, SimulationModeController, ConfSchema,
-    RuleSchema, BlockchainModeController, createBlockchainConf
+    SchemaDefs, Matcher, SimpleMatcher, ConfigurableReferenceMatcher, Controller, SimulationModeController, BlockchainModeController, createBlockchainConf
 }
