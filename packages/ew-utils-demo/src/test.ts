72cd532bebe8c1622c411f5798e9408e258f57f3
Chirag Mahaveer Parmar <chirag.parmar@slock.it>
2019-02-25 10:39:36 +0100

    startinstead of start-demo

--- packages/ew-utils-demo/src/test.ts
+++ packages/ew-utils-demo/src/test.ts
@@ -4,10 +4,13 @@ import * as fs from 'fs'
 
 async function main() {
     const contractConfig = await deployEmptyContracts()
-    console.log(contractConfig)
 
-    const demoFile = fs.readFileSync("./config/demo-config-2.json", 'utf8').toString()
+    //you could either use the default config file "demo-config.json"
     await marketDemo()
+
+    //or you could also pass a customized demo file to the demo function as depicted below
+    //const demoFile = fs.readFileSync("./config/demo-config-2.json", 'utf8').toString()
+    //await marketDemo(demoFile)
 }
 
 main()
