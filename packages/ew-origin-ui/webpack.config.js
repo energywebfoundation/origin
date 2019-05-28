7e75bcd5298b8decbd89206bcb37ae508d331327
Joseph Bagaric <joseph@josephit.com>
2019-05-28 14:38:42 +0200

    Fix CSS loader

--- packages/ew-origin-ui/webpack.config.js
+++ packages/ew-origin-ui/webpack.config.js
@@ -31,6 +31,8 @@ module.exports = {
             {
                 test: /\.(scss|css)$/,
                 use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
                     loader: "css-loader" // translates CSS into CommonJS
                 }, {
                     loader: "sass-loader" // compiles Sass to CSS
