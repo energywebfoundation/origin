757f65bbbe76601a1f0e05f7530575944243bc59
Daniel Kmak <neverkuzi@gmail.com>
2019-07-18 14:45:41 +0200

    [BUGFIX] Fix Docker deployment (#27)

--- packages/ew-origin-ui/webpack/base.config.js
+++ packages/ew-origin-ui/webpack/base.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: __dirname + "/../dist"
     },
 
     resolve: {
