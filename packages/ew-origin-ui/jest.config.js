19c1835fa204832ccfeed67d33129439910e9abb
Daniel Kmak <neverkuzi@gmail.com>
2019-07-22 12:07:01 +0200

    Setup integration test for producing assets table (#29)
    - Setup integration test for showing Producing Assets in table and going to Producing Asset Detail View
    - Reorganize jest config and use ts-jest

---
+++ packages/ew-origin-ui/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "node",
+    "json"
+  ],
+  testMatch: [
+    "**/__tests__/*.test.(ts|tsx|js)"
+  ],
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/src/__tests__/config/mocks/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/src/__tests__/config/mocks/fileMock.js"
+  },
+  setupFilesAfterEnv: [
+    "jest-enzyme",
+  ],
+  testEnvironment: "enzyme"
+};
