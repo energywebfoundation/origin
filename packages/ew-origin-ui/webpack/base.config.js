6563a61f5054a2fdb66dab1ceabafeba84363057
Piotr Kosiński <piotrkosinski@gmail.com>
2019-08-21 11:15:05 +0200

    remove react as external

--- packages/ew-origin-ui/webpack/base.config.js
+++ packages/ew-origin-ui/webpack/base.config.js
@@ -61,10 +61,10 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // },
     node: {
         fs: 'empty'
       }
