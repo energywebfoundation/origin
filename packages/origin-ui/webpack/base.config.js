b173b050385c26ddba10a494331c76a601775c95
Daniel Kmak <neverkuzi@gmail.com>
2019-09-03 16:19:21 +0200

    Faster incremental UI builds

--- packages/origin-ui/webpack/base.config.js
+++ packages/origin-ui/webpack/base.config.js
@@ -1,6 +1,6 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -32,7 +32,8 @@ module.exports = {
             meta: {
                 viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
             }
-        })
+        }),
+        new ForkTsCheckerWebpackPlugin()
     ],
     module: {
         rules: [
@@ -60,15 +61,15 @@ module.exports = {
                         }
                     }
                 ]
-            },
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            },     
             {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
                 options: {
-                    configFile: '../tsconfig.build.json'
+                    configFile: '../tsconfig.build.json',
+                    transpileOnly: true
                 }
-            },
+              },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: 'pre', test: /\.js\.map$/, loader: 'source-map-loader' }
