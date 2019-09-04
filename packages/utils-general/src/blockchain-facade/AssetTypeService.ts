79f3405d3bda498895e1d672eb3b2444d22b8699
Piotr Kosiński <piotrkosinski@gmail.com>
2019-09-04 12:15:13 +0200

    feat: remaining IREC structure

--- packages/utils-general/src/blockchain-facade/AssetTypeService.ts
+++ packages/utils-general/src/blockchain-facade/AssetTypeService.ts
@@ -115,6 +115,103 @@ export class IRECAssetService implements IAssetService {
                         type: 'Concentration'
                     }
                 ]
+            },
+            {
+                type: 'Wind',
+                subType: [{ type: 'Onshore' }, { type: 'Offshore' }]
+            },
+            {
+                type: 'Hydro-electric Head',
+                subType: [
+                    {
+                        type: 'Run-of-river head installation'
+                    },
+                    {
+                        type: 'Storage head installation'
+                    },
+                    {
+                        type: 'Pure pumped storage head installation'
+                    },
+                    {
+                        type: 'Mixed pumped storage head'
+                    }
+                ]
+            },
+            {
+                type: 'Marine',
+                subType: [
+                    {
+                        type: 'Tidal',
+                        subType: [{ type: 'Inshore' }, { type: 'Offshore' }]
+                    },
+                    {
+                        type: 'Wave',
+                        subType: [{ type: 'Onshore' }, { type: 'Offshore' }]
+                    },
+                    { type: 'Currents' },
+                    { type: 'Pressure' },
+                    { type: 'Thermal' }
+                ]
+            },
+            {
+                type: 'Solid',
+                subType: [
+                    {
+                        type: 'Muncipal waste',
+                        subType: [{ type: 'Biogenic' }]
+                    },
+                    {
+                        type: 'Industrial and commercial waste',
+                        subType: [{ type: 'Biogenic' }]
+                    },
+                    {
+                        type: 'Wood',
+                        subType: [
+                            { type: 'Forestry products' },
+                            { type: 'Forestry by-products & waste' }
+                        ]
+                    },
+                    { type: 'Animal fats' },
+                    {
+                        type: 'Biomass from agriculture',
+                        subType: [
+                            { type: 'Agricultural products' },
+                            { type: 'Agricultural by-products & waste' }
+                        ]
+                    }
+                ]
+            },
+            {
+                type: 'Liquid',
+                subType: [
+                    { type: 'Municipal biodegradable waste' },
+                    { type: 'Black liquor' },
+                    { type: 'Pure plant oil' },
+                    { type: 'Waste plant oil' },
+                    {
+                        type: 'Refined vegetable oil',
+                        subType: [
+                            { type: 'Biodiesel (mono-alkyl ester)' },
+                            { type: 'Biogasoline (C6-C12 hydrocarbon)' }
+                        ]
+                    }
+                ]
+            },
+            {
+                type: 'Gaseous',
+                subType: [
+                    { type: 'Landfill gas' },
+                    { type: 'Sewage gas' },
+                    {
+                        type: 'Agricultural gas',
+                        subType: [{ type: 'Animal manure' }, { type: 'Energy crops' }]
+                    },
+                    { type: 'Gas from organic waste digestion' },
+                    {
+                        type: 'Process gas',
+                        subType: [{ type: 'Biogenic' }]
+                    }
+                ]
             }
         ];
     }
