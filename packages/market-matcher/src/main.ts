32dab1386aa55b5bf95edec57e100ad8095498e6
Joseph Bagaric <joseph@josephit.com>
2019-10-02 14:03:13 +0200

    chore(market-matcher): cleanup

--- packages/market-matcher/src/main.ts
+++ packages/market-matcher/src/main.ts
@@ -10,16 +10,6 @@ dotenv.config({
 const privateKey = process.env.MATCHER_PRIV_KEY;
 const web3 = new Web3(process.env.WEB3);
 
-console.log({
-    web3Url: process.env.WEB3,
-    marketContractLookupAddress: process.env.MARKET_CONTRACT_ADDRESS,
-    matcherAccount: {
-        address: web3.eth.accounts.privateKeyToAccount(privateKey).address,
-        privateKey
-    },
-    offChainDataSourceUrl: process.env.BACKEND_URL
-});
-
 const config = {
     web3Url: process.env.WEB3,
     marketContractLookupAddress: process.env.MARKET_CONTRACT_ADDRESS,
