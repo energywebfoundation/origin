a46a7a69e8cb2dadf3139a616fb3e455ed6f7b82
Joseph Bagaric <joseph@josephit.com>
2019-10-03 18:49:45 +0200

    fix(event-listener): .env overwrite fix (#115)

--- packages/event-listener/src/index.ts
+++ packages/event-listener/src/index.ts
@@ -1,17 +1,10 @@
-import dotenv from 'dotenv';
 import Web3 from 'web3';
 
 import { EventServiceProvider } from './services/event.service';
 
-export { EventServiceProvider };
-
 export const SCAN_INTERVAL = 3000;
 
-(async () => {
-    dotenv.config({
-        path: '../../.env'
-    });
-
+const startEventListener = () => {
     const web3 = new Web3(process.env.WEB3 || 'http://localhost:8550');
 
     const eventService = new EventServiceProvider(process.env.BACKEND_URL, web3);
@@ -19,4 +12,6 @@ export const SCAN_INTERVAL = 3000;
     setInterval(async () => {
         await eventService.refreshListenerList();
     }, SCAN_INTERVAL);
-})();
+};
+
+export { startEventListener, EventServiceProvider };
