298fd460e9253b7a95305e5b1e2d56c7b5980133
Joseph Bagaric <joseph@josephit.com>
2019-10-14 23:48:47 +0900

    chore(monorepo): replace utils-testbackend references with origin-backend

--- packages/event-listener/src/services/event.service.ts
+++ packages/event-listener/src/services/event.service.ts
@@ -34,7 +34,7 @@ export class EventServiceProvider implements IEventServiceProvider {
     }
 
     public async refreshListenerList() {
-        const result = await axios.get(`${this.apiUrl}/MarketContractLookup`);
+        const result = await axios.get(`${this.apiUrl}/contract`);
 
         const latestMarketContracts = result.data;
         const currentlyListeningContracts = this.listeners.map(
