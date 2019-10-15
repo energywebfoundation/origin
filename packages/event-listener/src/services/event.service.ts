2c4ced30438bc550e4ea1b9f638ddba9d582f74b
Joseph Bagaric <joseph@josephit.com>
2019-10-15 19:03:18 +0200

    chore(monorepo): proper naming for MarketContractLookup

--- packages/event-listener/src/services/event.service.ts
+++ packages/event-listener/src/services/event.service.ts
@@ -34,7 +34,7 @@ export class EventServiceProvider implements IEventServiceProvider {
     }
 
     public async refreshListenerList() {
-        const result = await axios.get(`${this.apiUrl}/MarketLookupContract`);
+        const result = await axios.get(`${this.apiUrl}/MarketContractLookup`);
 
         const latestMarketContracts = result.data;
         const currentlyListeningContracts = this.listeners.map(
