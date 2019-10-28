35f295a238b8f51a95a206b890d812a65c5e1870
Daniel Kmak <neverkuzi@gmail.com>
2019-10-28 12:42:34 +0100

    fix(origin): fix getting all certificates events (#193)

--- packages/origin-ui/src/components/CertificateDetailView.tsx
+++ packages/origin-ui/src/components/CertificateDetailView.tsx
@@ -190,9 +190,10 @@ class CertificateDetailViewClass extends React.Component<Props, IDetailViewState
                     <span className="timestamp text-muted">
                         {new Date(event.timestamp * 1000).toLocaleString()} -{' '}
                         <a
-                            href={'https://tobalaba.etherscan.com/tx/' + event.txHash}
+                            href={`${process.env.BLOCKCHAIN_EXPLORER_URL}/tx/${event.txHash}`}
                             className="text-muted"
                             target="_blank"
+                            rel="noopener"
                         >
                             {event.txHash}
                         </a>
