224d54569f191cabfb9295b534abdc8e4f206bb6
Daniel Kmak <neverkuzi@gmail.com>
2019-11-13 23:58:28 +0100

    Bring back quotes in .env

--- packages/origin-ui/env.sh
+++ packages/origin-ui/env.sh
@@ -7,13 +7,6 @@ REQUIRED_VARIABLES=(
   BLOCKCHAIN_EXPLORER_URL
 )
 
-containsElement () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
 # Recreate config file
 rm -rf ./env-config.js
 touch ./env-config.js
@@ -27,35 +20,18 @@ if [ ! -e "$envToRead" ]; then
     envToRead=$rootEnvFile
 fi 
 
-if [ -e "$envToRead" ]; then
-  # Read each line in .env file
-  # Each line represents key=value pairs
-  while read -r line || [[ -n "$line" ]];
-  do
-    # Split env variables by character `=`
-    if printf '%s\n' "$line" | grep -q -e '='; then
-      varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-
-      if containsElement "$varname" "${REQUIRED_VARIABLES[@]}"; then
-        varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-
-        # Read value of current variable if exists as Environment variable
-        value=$(printf '%s\n' "${!varname}")
-        # Otherwise use value from .env file
-        [[ -z $value ]] && value=${varvalue}
-        
-        # Append configuration property to JS file
-        echo "    $varname: \"$value\"," >> ./env-config.js
-      fi
-    fi
-  done < $envToRead
-else
-  for i in "${REQUIRED_VARIABLES[@]}"
+for i in "${REQUIRED_VARIABLES[@]}"
   do
     varname="$i"
     value=$(printf '%s\n' "${!varname}")
+
+    if [ -z "$value" ]; then
+      if test -f $rootEnvFile; then
+        value=$(grep -e '^'$varname'=.*' $rootEnvFile | cut -d '=' -f2 | xargs)    
+      fi
+    fi
+
     echo "    $varname: \"$value\"," >> ./env-config.js
   done
-fi 
 
 echo "};" >> ./env-config.js
