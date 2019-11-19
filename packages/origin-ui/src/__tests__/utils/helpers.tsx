e78036d917d75c63fcc48f9aed50b6903e9b3a3b
Daniel Kmak <neverkuzi@gmail.com>
2019-11-19 21:14:06 +0100

    chore(origin-ui): update material design dependencies

--- packages/origin-ui/src/__tests__/utils/helpers.tsx
+++ packages/origin-ui/src/__tests__/utils/helpers.tsx
@@ -364,7 +364,7 @@ export const createRenderedHelpers = (rendered: ReactWrapper) => {
                     .value
             ).toBe('');
 
-            rendered.find(`#select-${name}`).simulate('click');
+            rendered.find(`#mui-component-select-${name}`).simulate('click');
 
             expect(
                 Array.from(document.querySelectorAll(`#menu-${name} ul li`)).map(i => i.textContent)
