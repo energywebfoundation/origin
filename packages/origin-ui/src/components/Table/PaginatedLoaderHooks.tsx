92db6d7b41f6d199909dd786b341706266706846
Daniel Kmak <neverkuzi@gmail.com>
2019-11-25 13:00:23 +0100

    chore(origin-ui): rewrite paginatedloader for certificate requests and reads to hooks (#301)

---
+++ packages/origin-ui/src/components/Table/PaginatedLoaderHooks.tsx
@@ -0,0 +1,53 @@
+import { ICustomFilter } from './FiltersHeader';
+import { PAGINATED_LOADER_INITIAL_STATE, DEFAULT_PAGE_SIZE } from './PaginatedLoader';
+import { useState } from 'react';
+
+export interface IPaginatedLoaderHooksFetchDataParameters {
+    requestedPageSize: number;
+    offset: number;
+    filters?: ICustomFilter[];
+}
+
+interface IUsePaginatedLoaderParameters<T> {
+    getPaginatedData: ({
+        requestedPageSize,
+        offset,
+        filters
+    }: IPaginatedLoaderHooksFetchDataParameters) => Promise<{
+        paginatedData: T[];
+        total: number;
+    }>;
+    initialPageSize?: number;
+}
+
+export function usePaginatedLoader<T>({
+    getPaginatedData,
+    initialPageSize = DEFAULT_PAGE_SIZE
+}: IUsePaginatedLoaderParameters<T>) {
+    const [paginatedData, setPaginatedData] = useState<T[]>(
+        PAGINATED_LOADER_INITIAL_STATE.paginatedData
+    );
+    const [total, setTotal] = useState(PAGINATED_LOADER_INITIAL_STATE.total);
+    const [pageSize, setPageSize] = useState(initialPageSize);
+
+    async function loadPage(page: number, filters?: ICustomFilter[]) {
+        const offset = (page - 1) * pageSize;
+
+        const { paginatedData: newPaginatedData, total: newTotal } = await getPaginatedData({
+            requestedPageSize: pageSize,
+            offset,
+            filters
+        });
+
+        setPaginatedData(newPaginatedData);
+        setTotal(newTotal);
+    }
+
+    return {
+        loadPage,
+        pageSize,
+        paginatedData,
+        setPageSize,
+        total
+    };
+}
