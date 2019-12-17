cfdea373dcc7181e7665bb9dfcdb961b314513ca
JosephBagaric <bagaricjos@gmail.com>
2019-12-17 12:36:47 +0100

    feat(origin-backend-client): add Compliance support

--- packages/origin-backend-client/src/ConfigurationClient.ts
+++ packages/origin-backend-client/src/ConfigurationClient.ts
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
-export type ConfigurationItem = 'MarketContractLookup' | 'Currency';
+export type ConfigurationItem = 'MarketContractLookup' | 'Currency' | 'Compliance';
 
 export interface IConfigurationClient {
-    get(baseUrl: string, item: ConfigurationItem): Promise<string[]>;
+    get(baseUrl: string, item: ConfigurationItem): Promise<string[] | string>;
     add(baseUrl: string, item: ConfigurationItem, value: string): Promise<boolean>;
 }
 
 export class ConfigurationClient implements IConfigurationClient {
     public async get(baseUrl: string, item: ConfigurationItem) {
-        const url = `${baseUrl}/${item.toLowerCase()}`;
-
+        const url = `${baseUrl}/${item}`;
+        
         const result = await axios.get(url);
 
-        return result.data as string[];
+        return result.data;
     }
 
     public async add(baseUrl: string, item: ConfigurationItem, value: string) {
-        const url = `${baseUrl}/${item.toLowerCase()}/${value}`;
+        const url = `${baseUrl}/${item}`;
 
-        const result = await axios.post(url);
+        const result = await axios.post(url, { value });
 
-        return result.status === 200;
+        return result.status >= 200 && result.status < 300;
     }
 }
