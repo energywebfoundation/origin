b42c0af75c526685b19c9951b6d467e42a7794cc
JosephBagaric <bagaricjos@gmail.com>
2019-12-19 19:18:49 +0100

    fix(origin-backend-client): make IConfigurationClient more general to support Country/Regions

--- packages/origin-backend-client/src/ConfigurationClientMock.ts
+++ packages/origin-backend-client/src/ConfigurationClientMock.ts
@@ -1,20 +1,24 @@
 import { IConfigurationClient, ConfigurationItem } from './ConfigurationClient';
 
 export class ConfigurationClientMock implements IConfigurationClient {
-    private storage = new Map<string, string[]>();
+    private storage = new Map<string, any>();
 
     public async get(baseUrl: string, item: ConfigurationItem) {
-        const url = `{baseUrl}/${item.toLowerCase()}`;
+        const url = `${baseUrl}/${item.toLowerCase()}`;
 
         return this.storage.get(url);
     }
 
-    public async add(baseUrl: string, item: ConfigurationItem, value: string) {
-        const url = `{baseUrl}/${item.toLowerCase()}`;
-        const values = this.storage.get(url) || [];
+    public async add(baseUrl: string, item: ConfigurationItem, value: string | object) {
+        const url = `${baseUrl}/${item.toLowerCase()}`;
 
-        values.push(value);
-        this.storage.set(url, values);
+        if (typeof value === 'string') {
+            const values = this.storage.get(url) || [];
+            values.push(value);
+            this.storage.set(url, values);
+        } else if (typeof value === 'object') {
+            this.storage.set(url, value);
+        }
 
         return true;
     }
