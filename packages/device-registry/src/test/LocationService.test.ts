f45defff4bf11ec54146a401d73f96bcd7b48f01
JosephBagaric <bagaricjos@gmail.com>
2019-12-20 10:41:57 +0100

    chore(device-registry): use updated LocationService

--- packages/device-registry/src/test/LocationService.test.ts
+++ packages/device-registry/src/test/LocationService.test.ts
@@ -2,30 +2,6 @@ import { assert } from 'chai';
 import { LocationService } from '../utils/LocationService';
 
 describe('LocationService tests', () => {
-    const expectedRegion = 'Central';
-    const expectedProvince = 'Nakhon Pathom';
-    const expectedCountry = 'Thailand';
-    const expected = `${expectedCountry};${expectedRegion};${expectedProvince}`;
-
-    const locationService = new LocationService(expectedCountry, { [expectedRegion]: [ expectedProvince ] });    
-
-    it('should translate existing device locations', () => {
-        const address =
-            '95 Moo 7, Sa Si Mum Sub-district, Kamphaeng Saen District, Nakhon Province 73140';
-
-        const expected = `${expectedCountry};${expectedRegion};${expectedProvince}`;
-
-        const result = locationService.translateAddress(address);
-        assert.equal(result, expected);
-    });
-
-    it('should translate based on province only', () => {
-        const address = 'Nakhon Province 73140';
-
-        const result = locationService.translateAddress(address);
-        assert.equal(result, expected);
-    });
-
     describe('matches()', () => {
         it('correctly matches cases with different specificity', () => {
             const TEST_MATRIX = [
@@ -45,7 +21,7 @@ describe('LocationService tests', () => {
 
             for (const [deviceTypes, typeToCheck, expectedResult] of TEST_MATRIX) {
                 assert.equal(
-                    locationService.matches(deviceTypes as string[], typeToCheck as string),
+                    new LocationService().matches(deviceTypes as string[], typeToCheck as string),
                     expectedResult
                 );
             }
--- packages/device-registry/src/utils/LocationService.ts
+++ packages/device-registry/src/utils/LocationService.ts
@@ -1,45 +1,4 @@
 export class LocationService {
-    constructor(public country: string, public regions: object) {
-        if (country !== 'Thailand') {
-            throw new Error('Not implemented');
-        }
-    }
-
-    public translateAddress(address: string) {
-        const clean = this.clear(address);
-
-        const zipRegex = /[0-9]{5}/;
-        const split = clean.split(',').reverse();
-        const provinceWithZip = (split[0] || '').trim();
-
-        const matchZipResult = provinceWithZip.match(zipRegex);
-        const zip = matchZipResult ? matchZipResult[0].trim() : '';
-
-        const splitResult = provinceWithZip.split(zipRegex);
-
-        const province = zip
-            ? (splitResult[0]?.trim() || splitResult[1]?.trim() || '').trim()
-            : provinceWithZip || '';
-
-        for (const [region, provinces] of Object.entries(this.regions)) {
-            const included = provinces.some((p: string) => p === province);
-
-            if (included) {
-                return `${this.country};${region};${province}`;
-            }
-
-            const alternativeProvince =
-                provinces.find((p: string) => province.includes(p)) ||
-                provinces.find((p: string) => p.includes(province));
-
-            if (alternativeProvince) {
-                return `${this.country};${region};${alternativeProvince}`;
-            }
-        }
-
-        throw new Error('unable to translate address');
-    }
-
     public matches(currentLocation: string[], checkedLocation: string) {
         const highestSpecificityTypes = this.filterForHighestSpecificity(
             currentLocation
@@ -56,12 +15,6 @@ export class LocationService {
 
     public decode(encoded: string[]): string[][] {
         return encoded.map(item => item.split(';'));
-    }
-
-    private clear(input: string) {
-        const terms = [['Nakhon Province', 'Nakhon Pathom']];
-
-        return terms.reduce((res, term) => `${res}`.replace(term[0], term[1] || ''), input);
     }
 
     private filterForHighestSpecificity(types: string[]): string[] {
