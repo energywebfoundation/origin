a6c6569f1afc2a55657800619590743f4e830b27
JosephBagaric <bagaricjos@gmail.com>
2019-12-20 14:32:59 +0100

    chore(origin-backend-client): don't lowercase urls in ConfigurationClientMock

--- packages/origin-backend-client/src/ConfigurationClientMock.ts
+++ packages/origin-backend-client/src/ConfigurationClientMock.ts
@@ -4,13 +4,13 @@ export class ConfigurationClientMock implements IConfigurationClient {
     private storage = new Map<string, any>();
 
     public async get(baseUrl: string, item: ConfigurationItem) {
-        const url = `${baseUrl}/${item.toLowerCase()}`;
+        const url = `${baseUrl}/${item}`;
 
         return this.storage.get(url);
     }
 
     public async add(baseUrl: string, item: ConfigurationItem, value: string | object) {
-        const url = `${baseUrl}/${item.toLowerCase()}`;
+        const url = `${baseUrl}/${item}`;
 
         if (typeof value === 'string') {
             const values = this.storage.get(url) || [];
