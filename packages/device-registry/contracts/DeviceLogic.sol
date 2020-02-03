075f81e26ff10b3ef468ccd0badb752e4d1ba695
JosephBagaric <bagaricjos@gmail.com>
2020-02-03 22:57:25 +0100

    chore: fix typo DevicManager

--- packages/device-registry/contracts/DeviceLogic.sol
+++ packages/device-registry/contracts/DeviceLogic.sol
@@ -95,13 +95,13 @@ contract DeviceLogic is Initializable, RoleManagement, IDeviceLogic {
         require(isRole(RoleManagement.Role.DeviceManager, _owner), "device owner has to have device manager role");
         require(
             _owner == msg.sender ||
-            isRole(RoleManagement.Role.DevicManager, msg.sender) ||
+            isRole(RoleManagement.Role.DeviceManager, msg.sender) ||
             isRole(RoleManagement.Role.DeviceAdmin, msg.sender) ||
             isRole(RoleManagement.Role.Issuer, msg.sender),
             "only device admin, manager and issuer can create a device for different owner"
         );
         require(
-            isRole(RoleManagement.Role.DevicManager, msg.sender) ||
+            isRole(RoleManagement.Role.DeviceManager, msg.sender) ||
             isRole(RoleManagement.Role.DeviceAdmin, msg.sender) ||
             isRole(RoleManagement.Role.Issuer, msg.sender), "only device admin, manager and issuer can add devices"
         );
