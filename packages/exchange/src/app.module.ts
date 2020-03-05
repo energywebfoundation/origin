720f5f155e2800a8b99da2dc10d3177fb63acbf8
Piotr Kosiński <piotrkosinski@gmail.com>
2020-03-05 09:04:19 +0100

    chore(exchange): read DB credentials from config files

--- packages/exchange/src/app.module.ts
+++ packages/exchange/src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import path from 'path';
 
 import { AppService } from './app.service';
+import { AccountBalanceModule } from './pods/account-balance/account-balance.module';
 import { AccountDeployerModule } from './pods/account-deployer/account-deployer.module';
 import { Account } from './pods/account/account.entity';
 import { AccountModule } from './pods/account/account.module';
@@ -12,6 +13,7 @@ import { Asset } from './pods/asset/asset.entity';
 import { AssetModule } from './pods/asset/asset.module';
 import { Demand } from './pods/demand/demand.entity';
 import { DemandModule } from './pods/demand/demand.module';
+import { DepositWatcherModule } from './pods/deposit-watcher/deposit-watcher.module';
 import { MatchingEngineModule } from './pods/matching-engine/matching-engine.module';
 import { OrderBookModule } from './pods/order-book/order-book.module';
 import { Order } from './pods/order/order.entity';
@@ -21,8 +23,6 @@ import { Trade } from './pods/trade/trade.entity';
 import { TradeModule } from './pods/trade/trade.module';
 import { Transfer } from './pods/transfer/transfer.entity';
 import { TransferModule } from './pods/transfer/transfer.module';
-import { AccountBalanceModule } from './pods/account-balance/account-balance.module';
-import { DepositWatcherModule } from './pods/deposit-watcher/deposit-watcher.module';
 import { WithdrawalProcessorModule } from './pods/withdrawal-processor/withdrawal-processor.module';
 
 const getEnvFilePath = () => {
@@ -34,15 +34,18 @@ const getEnvFilePath = () => {
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ envFilePath: getEnvFilePath(), isGlobal: true }),
+        ConfigModule.forRoot({
+            envFilePath: getEnvFilePath(),
+            isGlobal: true
+        }),
         TypeOrmModule.forRoot({
-            name: 'ExchangeConnection',
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'origin-exchange',
+            name: 'ExchangeConnection',
+            host: process.env.EXCHANGE_DB_HOST ?? 'localhost',
+            port: Number(process.env.EXCHANGE_DB_PORT) ?? 5432,
+            username: process.env.EXCHANGE_DB_USERNAME ?? 'postgres',
+            password: process.env.EXCHANGE_DB_PASSWORD ?? 'postgres',
+            database: process.env.EXCHANGE_DB_DATABASE ?? 'origin-exchange',
             entities: [Demand, Order, Trade, Asset, Transfer, Account],
             synchronize: true,
             logging: ['info']
