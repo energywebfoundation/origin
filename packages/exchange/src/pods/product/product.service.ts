f2e2a43f9f9983289d5d17b0f3764dd3e1bbf03f
Piotr Kosiński <piotrkosinski@gmail.com>
2020-03-13 13:53:58 +0100

    chore(exchange): add origin-backend dependency

--- packages/exchange/src/pods/product/product.service.ts
+++ packages/exchange/src/pods/product/product.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { DeviceService } from '@energyweb/origin-backend';
+
 import { ProductDTO } from '../order/product.dto';
 
 @Injectable()
 export class ProductService {
+    constructor(private readonly deviceService: DeviceService) {}
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public getProduct(deviceId: string): ProductDTO {
+    public getProduct(issuerId: string): ProductDTO {
         const product = new ProductDTO();
         product.deviceType = ['Solar;Photovoltaic;Classic silicon'];
         product.location = ['Thailand;Central;Nakhon Pathom'];
