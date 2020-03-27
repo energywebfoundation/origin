3208b4a9cc35d51f35776002903843d74d104d09
JosephBagaric <bagaricjos@gmail.com>
2020-03-27 09:35:53 +0100

    chore(origin-backend): add example migration

--- packages/origin-backend/src/config/configuration.ts
+++ packages/origin-backend/src/config/configuration.ts
@@ -1,4 +1,5 @@
 import { ConnectionOptions } from 'typeorm';
+import ormConfig from './ormconfig';
 
 interface IApplicationConfig {
     JWT_SECRET: string;
@@ -13,16 +14,7 @@ export default function createConfig(): IApplicationConfig {
         JWT_SECRET: process.env.JWT_SECRET ?? 'thisisnotsecret',
         JWT_EXPIRY_TIME: process.env.JWT_EXPIRY_TIME ?? '7 days',
         PASSWORD_HASH_COST: 8,
-        ORM: {
-            type: 'postgres',
-            host: process.env.DB_HOST ?? 'localhost',
-            port: Number(process.env.DB_PORT) ?? 5432,
-            username: process.env.DB_USERNAME ?? 'postgres',
-            password: process.env.DB_PASSWORD ?? 'postgres',
-            database: process.env.DB_DATABASE ?? 'origin',
-            synchronize: true,
-            logging: ['info']
-        } as ConnectionOptions,
+        ORM: ormConfig,
         REGISTRATION_MESSAGE_TO_SIGN:
             process.env.REGISTRATION_MESSAGE_TO_SIGN ?? 'I register as Origin user'
     };
---
+++ packages/origin-backend/src/config/ormconfig.ts
@@ -0,0 +1,20 @@
+import { ConnectionOptions } from 'typeorm';
+
+const ormConfig: ConnectionOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST ?? 'localhost',
+    port: Number(process.env.DB_PORT) ?? 5432,
+    username: process.env.DB_USERNAME ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    database: process.env.DB_DATABASE ?? 'origin',
+    synchronize: true,
+    logging: ['info'],
+    migrationsRun: true,
+    logger: 'file',
+    migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+    cli: {
+        migrationsDir: 'src/migrations'
+    }
+};
+
+export = ormConfig;
