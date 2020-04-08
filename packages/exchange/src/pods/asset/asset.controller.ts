c24d6e458854e50e6107bf24007ff0ea41cd53f3
Piotr Kosiński <piotrkosinski@gmail.com>
2020-04-08 12:45:25 +0200

    chore(exchange): rm auth for asset endpoint

--- packages/exchange/src/pods/asset/asset.controller.ts
+++ packages/exchange/src/pods/asset/asset.controller.ts
@@ -4,10 +4,8 @@ import {
     Get,
     Param,
     ParseUUIDPipe,
-    UseGuards,
     UseInterceptors
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 
 import { AssetService } from './asset.service';
 
@@ -17,7 +15,6 @@ export class AssetController {
     constructor(private readonly assetService: AssetService) {}
 
     @Get('/:id')
-    @UseGuards(AuthGuard())
     public get(@Param('id', new ParseUUIDPipe({ version: '4' })) assetId: string) {
         return this.assetService.get(assetId);
     }
