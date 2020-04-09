361df8cd7aab120ecb56741ba58335c4264810a1
Piotr Kosiński <piotrkosinski@gmail.com>
2020-04-09 18:15:15 +0200

    chore(migrations): use DATABASE_URL if available, rename bin to origin-migrations

--- packages/migrations/src/main.ts
+++ packages/migrations/src/main.ts
@@ -48,13 +48,18 @@ const seedFilePath = absolutePath(program.seedFile ?? '../config/seed.sql');
         throw new Error('At least one currency has to be specified: e.g. [ "USD" ]');
     }
 
-    const postgresConfig: ClientConfig = {
-        host: process.env.DB_HOST ?? 'localhost',
-        port: Number(process.env.DB_PORT) || 5432,
-        user: process.env.DB_USERNAME ?? 'postgres',
-        password: process.env.DB_PASSWORD ?? 'postgres',
-        database: process.env.DB_DATABASE ?? 'origin'
-    };
+    const postgresConfig: ClientConfig = process.env.DATABASE_URL
+        ? {
+              connectionString: process.env.DATABASE_URL,
+              ssl: { rejectUnauthorized: false }
+          }
+        : {
+              host: process.env.DB_HOST ?? 'localhost',
+              port: Number(process.env.DB_PORT) || 5432,
+              user: process.env.DB_USERNAME ?? 'postgres',
+              password: process.env.DB_PASSWORD ?? 'postgres',
+              database: process.env.DB_DATABASE ?? 'origin'
+          };
     const client = new Client(postgresConfig);
 
     await client.connect();
