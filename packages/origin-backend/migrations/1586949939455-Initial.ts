77c497631ad683e0c32c6c4962469e004444e5f9
Piotr Kosiński <piotrkosinski@gmail.com>
2020-04-15 16:40:24 +0200

    chore(origin-backend): use typeorm migrations instead of custom ones

---
+++ packages/origin-backend/migrations/1586949939455-Initial.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import fs from 'fs';
+import path from 'path';
+
+export class Initial1586949939455 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        const table = await queryRunner.getTable('configuration');
+
+        if (table) {
+            console.log(
+                'configuration table exists. assuming initial migration was completed before'
+            );
+            return;
+        }
+
+        const initialPath = path.resolve(__dirname, './initial.sql');
+        const initialSqlFile = fs.readFileSync(initialPath).toString();
+
+        await queryRunner.query(initialSqlFile);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {}
+}
