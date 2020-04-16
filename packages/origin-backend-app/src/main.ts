224204fd3a15829b408416e976dc3470c01e7202
Piotr Kosiński <piotrkosinski@gmail.com>
2020-04-16 12:42:32 +0200

    chore(origin-backend-app): print packgae version on start

--- packages/origin-backend-app/src/main.ts
+++ packages/origin-backend-app/src/main.ts
@@ -2,14 +2,28 @@ import { AppModule as ExchangeModule } from '@energyweb/exchange';
 import { LoggerService } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
+import fs from 'fs';
 
 import { OriginAppModule } from './origin-app.module';
 import * as PortUtils from './port';
 
 export async function startAPI(logger?: LoggerService) {
     const PORT = PortUtils.getPort();
+    const getVersion = () => {
+        let info;
+        if (fs.existsSync('package.json')) {
+            info = fs.readFileSync('package.json');
+        } else if (fs.existsSync('../package.json')) {
+            info = fs.readFileSync('../package.json');
+        } else {
+            return 'unknown';
+        }
+
+        return JSON.parse(info.toString()).version;
+    };
 
     console.log(`Backend starting on port: ${PORT}`);
+    console.log(`Backend version: ${getVersion()}`);
 
     const app = await NestFactory.create(OriginAppModule.register(null));
     app.enableCors();
