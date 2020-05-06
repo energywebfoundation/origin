2035a822a6d5b5578b4d270649838ed251fb9059
JosephBagaric <bagaricjos@gmail.com>
2020-05-06 13:31:24 +0200

    chore(origin-backend): remove duplicate migrations folder

--- packages/origin-backend/src/migrations/1585294497417-ExampleMigration.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class ExampleMigration1585294497417 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<any> {}
-
-    public async down(queryRunner: QueryRunner): Promise<any> {}
-}
