ed18baa0cf046cde9daaa480a84726098ee8dabd
Piotr Kosiński <piotrkosinski@gmail.com>
2020-05-18 15:34:36 +0200

    fix(exchange): amount serialization

--- packages/exchange/src/pods/account-balance/account-balance.ts
+++ packages/exchange/src/pods/account-balance/account-balance.ts
@@ -1,8 +1,12 @@
+import { Type } from 'class-transformer';
+
 import { AccountAsset } from './account-asset';
 
 export class AccountBalance {
+    @Type(() => AccountAsset)
     public readonly available: AccountAsset[];
 
+    @Type(() => AccountAsset)
     public readonly locked: AccountAsset[];
 
     public constructor(accountBalance: Partial<AccountBalance>) {
