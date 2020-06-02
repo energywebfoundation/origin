58e408e99d7c4fc67bc9332e3a1a85945683ccbe
Piotr Kosiński <piotrkosinski@gmail.com>
2020-06-02 15:34:46 +0200

    chore(exchange-core): simplify TradeExecutedEvent

--- packages/exchange-core/src/TradeExecutedEvent.ts
+++ packages/exchange-core/src/TradeExecutedEvent.ts
@@ -1,8 +1,5 @@
 import { Trade } from './Trade';
-import { Ask } from './Ask';
-import { DirectBuy } from './DirectBuy';
-import { Bid } from './Bid';
 
 export class TradeExecutedEvent {
-    constructor(public trade: Trade, public ask: Ask, public bid: Bid | DirectBuy) {}
+    constructor(public readonly trade: Trade) {}
 }
