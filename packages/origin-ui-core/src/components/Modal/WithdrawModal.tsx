9cfa1a217b665a8b540d780bdf203f560fbb10a5
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-06-08 08:37:15 +0300

    feat(origin-ui-core): add WithdrawModal

--- packages/origin-ui-core/src/components/Modal/WithdrawModal.tsx
+++ packages/origin-ui-core/src/components/Modal/WithdrawModal.tsx
@@ -5,11 +5,6 @@ import {
     DialogActions,
     DialogContent,
     DialogTitle,
-    FilledInput,
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
     TextField
 } from '@material-ui/core';
 import { bigNumberify } from 'ethers/utils';
@@ -19,9 +14,9 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { resyncCertificate, requestWithdrawCertificate } from '../../features/certificates';
 import { ICertificateViewItem } from '../../features/certificates/types';
-import { getCurrencies, getExchangeClient } from '../../features/general/selectors';
+import { getExchangeClient } from '../../features/general/selectors';
 import { getUserOffchain } from '../../features/users/selectors';
-import { countDecimals, EnergyFormatter, formatDate } from '../../utils';
+import { EnergyFormatter, formatDate } from '../../utils';
 
 interface IProps {
     certificate: ICertificateViewItem;
@@ -35,19 +30,11 @@ const DEFAULT_ENERGY_IN_BASE_UNIT = bigNumberify(1);
 export function WithdrawModal(props: IProps) {
     const { certificate, callback, producingDevice, showModal } = props;
 
-    const currencies = useSelector(getCurrencies);
     const user = useSelector(getUserOffchain);
 
     const [energyInDisplayUnit, setEnergyInDisplayUnit] = useState(
         EnergyFormatter.getValueInDisplayUnit(DEFAULT_ENERGY_IN_BASE_UNIT)
     );
-
-    const [price, setPrice] = useState('1');
-    const [currency, setCurrency] = useState(null);
-    const [validation, setValidation] = useState({
-        energyInDisplayUnit: true,
-        price: true
-    });
 
     const dispatch = useDispatch();
 
@@ -60,14 +47,6 @@ export function WithdrawModal(props: IProps) {
             );
         }
     }, [certificate, user]);
-
-    useEffect(() => {
-        if (currency === null && currencies && currencies[0]) {
-            setCurrency(currencies[0]);
-        }
-    }, [currencies]);
-
-    const isFormValid = validation.energyInDisplayUnit && validation.price;
 
     async function handleClose() {
         dispatch(resyncCertificate(certificate));
@@ -89,41 +68,6 @@ export function WithdrawModal(props: IProps) {
         );
     }
 
-    async function validateInputs(event) {
-        switch (event.target.id) {
-            case 'energyInDisplayUnitInput':
-                const newEnergyInDisplayUnit = Number(event.target.value);
-                const newEnergyInBaseValueUnit = EnergyFormatter.getBaseValueFromValueInDisplayUnit(
-                    newEnergyInDisplayUnit
-                );
-
-                const ownedPublicVolume = certificate.energy.publicVolume;
-
-                const energyInDisplayUnitValid =
-                    newEnergyInBaseValueUnit.gte(1) &&
-                    newEnergyInBaseValueUnit.lt(ownedPublicVolume) &&
-                    countDecimals(newEnergyInDisplayUnit) <= 6;
-
-                setEnergyInDisplayUnit(newEnergyInDisplayUnit);
-
-                setValidation({
-                    ...validation,
-                    energyInDisplayUnit: energyInDisplayUnitValid
-                });
-                break;
-            case 'priceInput':
-                const newPrice = Number(event.target.value);
-                const priceValid = !isNaN(newPrice) && newPrice > 0 && countDecimals(newPrice) <= 2;
-
-                setPrice(event.target.value);
-                setValidation({
-                    ...validation,
-                    price: priceValid
-                });
-                break;
-        }
-    }
-
     const certificateId = certificate ? certificate.id : '';
     const facilityName = producingDevice ? producingDevice.facilityName : '';
 
@@ -132,7 +76,7 @@ export function WithdrawModal(props: IProps) {
     try {
         creationTime = certificate && formatDate(moment.unix(certificate.creationTime), true);
     } catch (error) {
-        console.error('Error in PublishForSaleModal', error);
+        console.error('Error: Can not get creation time', error);
     }
 
     return (
@@ -156,54 +100,17 @@ export function WithdrawModal(props: IProps) {
                 <TextField
                     label={EnergyFormatter.displayUnit}
                     value={energyInDisplayUnit}
-                    type="number"
-                    placeholder="1"
-                    onChange={(e) => validateInputs(e)}
                     className="mt-4"
                     id="energyInDisplayUnitInput"
                     fullWidth
+                    disabled
                 />
-
-                <TextField
-                    label="Price"
-                    value={price}
-                    type="number"
-                    placeholder="1"
-                    onChange={(e) => validateInputs(e)}
-                    className="mt-4"
-                    id="priceInput"
-                    fullWidth
-                />
-
-                <FormControl fullWidth={true} variant="filled" className="mt-4">
-                    <InputLabel>Currency</InputLabel>
-                    <Select
-                        value={currency}
-                        onChange={(e) => setCurrency(e.target.value as string)}
-                        fullWidth
-                        variant="filled"
-                        input={<FilledInput />}
-                    >
-                        {currencies?.map((item) => (
-                            <MenuItem key={item} value={item}>
-                                {item}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-
-                <div className="text-danger">
-                    {!validation.price && <div>Price is invalid</div>}
-                    {!validation.energyInDisplayUnit && (
-                        <div>{EnergyFormatter.displayUnit} value is invalid</div>
-                    )}
-                </div>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="secondary">
                     Cancel
                 </Button>
-                <Button onClick={withdraw} color="primary" disabled={!isFormValid}>
+                <Button onClick={withdraw} color="primary">
                     Withdraw
                 </Button>
             </DialogActions>
