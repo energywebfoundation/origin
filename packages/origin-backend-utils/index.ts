3626adae4173895d6ddb6886187eb347d900d86d
m0azam <izmadikram@gmail.com>
2020-06-08 16:49:05 +0800

    feat(origin-backend-utils):add new checking based on user status

--- packages/origin-backend-utils/index.ts
+++ packages/origin-backend-utils/index.ts
@@ -1,4 +1,4 @@
 export * from './src/decorator/UserDecorator';
 export * from './src/decorator/RolesDecorator';
 export * from './src/guard/RolesGuard';
-export * from './src/guard/UserGuardWithJWT';
+export * from './src/guard/UserGuard';
