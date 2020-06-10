63db40261b96f27d015b4ab2463a87a14b1af86d
m0azam <izmadikram@gmail.com>
2020-06-10 13:58:15 +0800

    chore(origin-backend):update all user.status to active

---
+++ packages/origin-backend/migrations/1591766102307-UpdateAllUserStatusActive.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdateAllUserStatusActive1591766102307 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`UPDATE "user" SET status = 1`, undefined);
+    }
+}
