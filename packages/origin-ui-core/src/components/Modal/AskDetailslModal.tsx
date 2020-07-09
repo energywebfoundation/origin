70d95240474ba8bb7693bea097c5655a758e7557
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-07-09 14:39:27 +0300

    feat(origin-ui-core): style order details modal

--- packages/origin-ui-core/src/components/Modal/AskDetailslModal.tsx
+++ packages/origin-ui-core/src/components/Modal/AskDetailslModal.tsx
@@ -22,14 +22,14 @@ export const AskDetailsModal = (props: IOwnProps) => {
     const { ask, close, cancelAsk } = props;
     const {
         filled,
-        product: { generationFrom, generationTo, externalDeviceId }
+        product: { generationFrom, generationTo, externalDeviceId, location }
     } = ask;
     const {
         spacing,
         palette: {
             text: { primary: textPrimary }
         },
-        typography: { fontSizeSm }
+        typography: { fontSizeSm, fontSizeLg }
     } = useTheme();
     const currency = useSelector(getCurrencies)[0];
     const environment = useSelector(getEnvironment);
@@ -40,62 +40,133 @@ export const AskDetailsModal = (props: IOwnProps) => {
     const { t } = useTranslation();
 
     return (
-        <Dialog open={ask !== null} onClose={close} style={{ fontSize: fontSizeSm }}>
-            <Grid container direction="column" style={{ padding: spacing(2) }}>
-                <Grid item style={{ alignSelf: 'end' }}>
-                    <IconButton onClick={close}>
-                        <CloseIcon style={{ color: textPrimary }} />
-                    </IconButton>
-                </Grid>
-                <Grid item style={{ alignSelf: 'start' }}>
-                    <Box color="text.primary">
-                        {moment(ask.validFrom).format('MMM Do, YYYY h:mm:ss a')}
-                    </Box>
-                </Grid>
-                <Grid item container style={{ paddingBottom: spacing(1) }}>
-                    <Grid item>
-                        <Box>Order No.</Box>
-                        <Box>{ask.id}</Box>
+        <Dialog
+            open={ask !== null}
+            onClose={close}
+            style={{ fontSize: fontSizeSm }}
+            className="OrderModal"
+            fullWidth={true}
+        >
+            <Grid container direction="column" style={{ alignItems: 'stretch' }}>
+                <Grid
+                    item
+                    container
+                    direction="column"
+                    style={{ paddingLeft: spacing(2), paddingRight: spacing(2) }}
+                >
+                    <Grid item style={{ alignSelf: 'end' }}>
+                        <IconButton onClick={close}>
+                            <CloseIcon style={{ color: textPrimary }} />
+                        </IconButton>
                     </Grid>
-                    <Grid item>
-                        <Box>{formatCurrencyComplete(ask.price / 100, currency)}</Box>
+                    <Grid item style={{ alignSelf: 'start' }}>
+                        <Box pb={1} fontWeight="fontWeightLight">
+                            {moment(ask.validFrom).format('MMM Do, YYYY h:mm:ss a')}
+                        </Box>
+                    </Grid>
+                    <Grid item container style={{ paddingBottom: spacing(1) }}>
+                        <Grid item xs>
+                            <Box color="text.secondary">Order No.</Box>
+                            <Box fontWeight="fontWeightLight">{ask.id}</Box>
+                        </Grid>
+                        <Grid item xs>
+                            <Box
+                                textAlign="right"
+                                fontWeight="fontWeightBold"
+                                fontSize={fontSizeLg}
+                            >
+                                {formatCurrencyComplete(ask.price / 100, currency)}
+                            </Box>
+                        </Grid>
                     </Grid>
                 </Grid>
+
                 <Grid
                     container
                     direction="row"
                     justify="space-between"
-                    style={{ paddingTop: spacing(1), paddingBottom: spacing(1) }}
+                    style={{
+                        paddingTop: spacing(1),
+                        paddingBottom: spacing(1),
+                        paddingLeft: spacing(2),
+                        paddingRight: spacing(2)
+                    }}
+                    className="EnergySection"
                 >
                     <Grid item>
-                        <Box>{EnergyFormatter.format(ask.currentVolume, true)}</Box>
-                        <Box>{deviceType}</Box>
+                        <Box fontWeight="fontWeightBold" fontSize={fontSizeLg}>
+                            {EnergyFormatter.format(ask.currentVolume, true)}
+                        </Box>
+                        <Box style={{ textTransform: 'capitalize' }}>{deviceType}</Box>
                     </Grid>
                     <Grid item>
-                        <Box>
+                        <Box textAlign="right" color="text.secondary">
                             {moment(generationFrom).format('MMM, YYYY')}
                             <ArrowRightAltIcon />
                             {moment(generationTo).format('MMM, YYYY')}
                         </Box>
                     </Grid>
                 </Grid>
-                <Grid container style={{ paddingTop: spacing(1), paddingBottom: spacing(1) }}>
-                    <Grid item>
-                        <Box>{t('device.properties.deviceType')}</Box>
-                        <Box fontWeight="fontWeightBold">{deviceType}</Box>
+
+                <Box mx={1} my={1}>
+                    <Grid
+                        container
+                        style={{
+                            paddingTop: spacing(1),
+                            paddingBottom: spacing(1),
+                            paddingLeft: spacing(1),
+                            paddingRight: spacing(1)
+                        }}
+                        className="DeviceSection"
+                    >
+                        <Grid item xs={4}>
+                            <Box color="text.secondary">{t('device.properties.deviceType')}</Box>
+                            <Box
+                                fontWeight="fontWeightBold"
+                                style={{ textTransform: 'capitalize' }}
+                            >
+                                {deviceType}
+                            </Box>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Box color="text.secondary">{t('device.properties.gridOperator')}</Box>
+                            <Box fontWeight="fontWeightBold">{ask.product.gridOperator}</Box>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Box textAlign="right" color="text.secondary">
+                                {t('device.properties.region')}
+                            </Box>
+                            <Box textAlign="right" fontWeight="fontWeightBold">
+                                {location[0].split(';')[1]}
+                            </Box>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <Box>{t('device.properties.gridOperator')}</Box>
-                        <Box fontWeight="fontWeightBold">{ask.product.gridOperator}</Box>
-                    </Grid>
-                    <Grid item></Grid>
-                </Grid>
-                <Grid item style={{ alignSelf: 'start' }}>
-                    <Box>{t('order.properties.filled')}</Box>
-                    <Box>{filled || '-'}</Box>
+                </Box>
+
+                <Grid
+                    item
+                    className="FilledSection"
+                    style={{
+                        paddingTop: spacing(1),
+                        paddingBottom: spacing(1),
+                        paddingLeft: spacing(2),
+                        paddingRight: spacing(2)
+                    }}
+                >
+                    <Box color="text.secondary">{t('order.properties.filled')}</Box>
+                    <Box color="text.secondary">{filled || '-'}</Box>
                 </Grid>
                 <Divider />
-                <Grid item style={{ alignSelf: 'end' }}>
+                <Grid
+                    item
+                    style={{
+                        textAlign: 'end',
+                        paddingTop: spacing(1),
+                        paddingBottom: spacing(1),
+                        paddingRight: spacing(1)
+                    }}
+                    className="Action"
+                >
                     <Button variant="contained" color="primary" onClick={() => cancelAsk(ask)}>
                         {t('order.actions.remove')}
                     </Button>
