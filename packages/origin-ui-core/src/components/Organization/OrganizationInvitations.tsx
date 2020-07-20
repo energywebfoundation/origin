061a5dd6ff77fe99b64dfa0d42c909587381b519
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-07-20 16:46:21 +0300

    fix(origin-ui-core): lint

--- packages/origin-ui-core/src/components/Organization/OrganizationInvitations.tsx
+++ packages/origin-ui-core/src/components/Organization/OrganizationInvitations.tsx
@@ -4,17 +4,10 @@ import { useSelector } from 'react-redux';
 import { getUserOffchain } from '../../features/users/selectors';
 import { OrganizationInvitationTable } from './OrganizationInvitationTable';
 import { isRole, Role } from '@energyweb/origin-backend-core';
-import { useTranslation } from '../../utils';
-import { Redirect } from 'react-router-dom';
 import { UserLogin } from '../Account/UserLogin';
 
 export function OrganizationInvitations() {
-    const { t } = useTranslation();
     const user = useSelector(getUserOffchain);
-
-    // if (!userOffchain) {
-    //     return t('general.feedback.registerOrLoginTryAgain');
-    // }
 
     if (!user) {
         return <UserLogin redirect="/organization/organization-invitations" />;
