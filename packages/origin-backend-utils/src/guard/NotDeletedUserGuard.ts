f2abddac77e9689b966b31217567c7e9a55ee5b8
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-07-22 16:11:41 +0300

    chore(origin-backend-utlis): use 403 instead of 412

--- packages/origin-backend-utils/src/guard/NotDeletedUserGuard.ts
+++ packages/origin-backend-utils/src/guard/NotDeletedUserGuard.ts
@@ -1,22 +1,20 @@
 import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { IUserWithRelationsIds, UserStatus } from '@energyweb/origin-backend-core';
+import { UserStatus } from '@energyweb/origin-backend-core';
 
 @Injectable()
 export class NotDeletedUserGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
     canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest();
-        const user = request.user as IUserWithRelationsIds;
-        const _user = user as IUserWithRelationsIds;
+        const { user } = context.switchToHttp().getRequest();
 
-        if (_user.status === UserStatus.Deleted) {
+        if (user.status === UserStatus.Deleted) {
             throw new HttpException(
                 `Only not deleted users can perform this action. Your status is ${
-                    UserStatus[_user.status]
+                    UserStatus[user.status]
                 }`,
-                412
+                403
             );
         }
 
