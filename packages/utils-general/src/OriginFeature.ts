86b86819227cacbe361ef75f1c425311e1770ec1
JosephBagaric <bagaricjos@gmail.com>
2020-07-24 12:04:49 +0200

    feat(utils-general): add support for Origin Features

---
+++ packages/utils-general/src/OriginFeature.ts
@@ -0,0 +1,9 @@
+export enum OriginFeature {
+    Bundles = 'bundles',
+    Exchange = 'exchange',
+    Seller = 'seller',
+    Buyer = 'buyer',
+    Devices = 'devices',
+    Certificates = 'certificates',
+    CertificationRequests = 'certificationRequests'
+}
