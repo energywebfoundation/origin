ca0a612c04addb3497a8f4a1def95e3e9640cdd3
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-08-03 13:49:30 +0300

    feat(origin-backend-utils): add IntUnitsOfEnergy validator

---
+++ packages/origin-backend-utils/src/validator/intUnitOfEnergy.ts
@@ -0,0 +1,25 @@
+import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+import BN from 'bn.js';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@ValidatorConstraint()
+@Injectable()
+export class IntUnitsOfEnergy implements ValidatorConstraintInterface {
+    private energyPerUnit: BN;
+
+    constructor(private readonly configService: ConfigService) {
+        this.energyPerUnit = new BN(this.configService.get<string>('ENERGY_PER_UNIT'));
+    }
+
+    validate(volume: string) {
+        if (new BN(volume).mod(this.energyPerUnit).isZero()) {
+            return true;
+        }
+        return false;
+    }
+
+    defaultMessage() {
+        return 'Energy volume must be integer number of energy units';
+    }
+}
