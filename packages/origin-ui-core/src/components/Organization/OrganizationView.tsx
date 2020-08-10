707044046127ebd601a16e04d840aab1fef81491
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-08-10 12:58:09 +0300

    chore(origin-ui-core): use organization from user

--- packages/origin-ui-core/src/components/Organization/OrganizationView.tsx
+++ packages/origin-ui-core/src/components/Organization/OrganizationView.tsx
@@ -1,36 +1,11 @@
-import { useParams } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { IOrganization } from '@energyweb/origin-backend-core';
 
 import { OrganizationForm } from './OrganizationForm';
-import { getOffChainDataSource } from '../../features/general/selectors';
 import { getUserOffchain } from '../../features/users/selectors';
-
-interface IMatchParams {
-    key?: string;
-    id?: string;
-}
 
 export function OrganizationView() {
     const userOffchain = useSelector(getUserOffchain);
-    const organizationClient = useSelector(getOffChainDataSource)?.organizationClient;
 
-    const [entity, setEntity] = useState<IOrganization>(null);
-
-    const params: IMatchParams = useParams();
-
-    async function fetchEntity(id: string | number) {
-        if (!organizationClient) {
-            return setEntity(null);
-        }
-
-        setEntity(await organizationClient.getById(Number(id)));
-    }
-
-    useEffect(() => {
-        fetchEntity(params?.id ?? userOffchain?.organization?.id);
-    }, [params, organizationClient]);
-
-    return <OrganizationForm entity={entity} readOnly={true} />;
+    return <OrganizationForm entity={userOffchain?.organization} readOnly={true} />;
 }
