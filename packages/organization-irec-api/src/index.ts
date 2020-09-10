1c446e1e0016b1bddbc8491e873f60d475a26bde
Piotr Kosiński <piotrkosinski@gmail.com>
2020-09-10 12:42:53 +0200

    chore(origin-organization-irec-api): rm dependencies, export modules

---
+++ packages/organization-irec-api/src/index.ts
@@ -0,0 +1,6 @@
+import { Connect } from './connect/connect.entity';
+import { Registration } from './registration/registration.entity';
+
+export * from './app.module';
+
+export const entities = [Connect, Registration];
