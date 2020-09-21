3b57e303ba73cc1e4e4117034ed523662a4c0acf
Piotr Kosiński <piotrkosinski@gmail.com>
2020-09-21 15:18:39 +0200

    fix(origin-backend-app): missing env parameters for smoke test

--- packages/origin-backend-app/test/smoke.e2e-spec.ts
+++ packages/origin-backend-app/test/smoke.e2e-spec.ts
@@ -15,7 +15,14 @@ describe('Origin Backend App', () => {
         const testLogger = new Logger('e2e');
 
         const configService = new ConfigService({
-            WEB3: 'http://localhost:8545'
+            WEB3: 'http://localhost:8545',
+            JWT_SECRET: '123',
+            EXCHANGE_ACCOUNT_DEPLOYER_PRIV:
+                '0xd9066ff9f753a1898709b568119055660a77d9aae4d7a4ad677b8fb3d2a571e5',
+            EXCHANGE_WALLET_PUB: '0xD173313A51f8fc37BcF67569b463abd89d81844f',
+            EXCHANGE_WALLET_PRIV:
+                '0xd9066ff9f753a1898709b568119055660a77d9aae4d7a4ad677b8fb3d2a571e5',
+            EXCHANGE_PRICE_STRATEGY: 0
         });
 
         const moduleFixture = await Test.createTestingModule({
