5a8b407a9bb8e832d0078656e839f5f618c8895b
Piotr Kosiński <piotrkosinski@gmail.com>
2020-09-25 09:38:42 +0200

    fix(origin-organizatin-irec-api): opt-in use of pipes and interceptors

--- packages/origin-organization-irec-api/src/app.module.ts
+++ packages/origin-organization-irec-api/src/app.module.ts
@@ -1,21 +1,8 @@
-import {
-    HTTPLoggingInterceptor,
-    NullOrUndefinedResultInterceptor
-} from '@energyweb/origin-backend-utils';
-import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { Module } from '@nestjs/common';
 
 import { RegistrationModule } from './registration/registration.module';
 
-export const providers = [
-    { provide: APP_PIPE, useClass: ValidationPipe },
-    { provide: APP_INTERCEPTOR, useClass: NullOrUndefinedResultInterceptor },
-    { provide: APP_INTERCEPTOR, useClass: HTTPLoggingInterceptor },
-    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor }
-];
-
 @Module({
-    imports: [RegistrationModule],
-    providers
+    imports: [RegistrationModule]
 })
 export class AppModule {}
