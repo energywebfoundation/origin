47a96ade1e1f6d555a8773a3e3453257d36c0e12
Alexander Popadyuk <alexander.popadyuk@482.solutions>
2020-10-13 14:56:47 +0300

    feat(origin-ui-core): add demand table to My Orders

--- packages/origin-ui-core/src/components/orders/MyOrders.tsx
+++ packages/origin-ui-core/src/components/orders/MyOrders.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { OrderSide, OrderStatus } from '@energyweb/exchange-core';
 
-import { Order } from '../../utils/exchange';
-import { getOrders } from '../../features/orders/selectors';
+import { Order, Demand, DemandStatus } from '../../utils/exchange';
+import { getOrders, getDemands } from '../../features/orders/selectors';
 import { BidsTable } from './BidsTable';
 import { AsksTable } from './AsksTable';
 import { Box } from '@material-ui/core';
+import { DemandsTable } from './DemandsTable';
 
 export const MyOrders = () => {
+    const demands: Demand[] = useSelector(getDemands).filter(
+        (d) => d.status !== DemandStatus.ARCHIVED
+    );
     const orders: Order[] = useSelector(getOrders).filter(
         (o) => o.status === OrderStatus.Active || o.status === OrderStatus.PartiallyFilled
     );
     const asks = orders.filter((o) => o.side === OrderSide.Ask);
-    const bids = orders.filter((o) => o.side === OrderSide.Bid);
+    const bids = orders.filter(
+        (o) => o.side === OrderSide.Bid && !{}.hasOwnProperty.call(o, 'demandId')
+    );
     return (
         <Box className="OpenOrders">
+            <Box mt={3}>
+                <DemandsTable demands={demands} />
+            </Box>
             <Box>
                 <BidsTable bids={bids} />
             </Box>
