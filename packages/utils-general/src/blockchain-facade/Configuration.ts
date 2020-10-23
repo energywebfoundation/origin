8e9b760f6f10036b73ce324e017fc2e7f6a64e48
Joseph Bagaric <bagaricjos@gmail.com>
2020-10-23 11:43:30 +0200

    chore(utils-general): Remove BlockchainProperties from Configuration
    BREAKING CHANGE: Configuration entity no longer contains BlockchainProperties

--- packages/utils-general/src/blockchain-facade/Configuration.ts
+++ packages/utils-general/src/blockchain-facade/Configuration.ts
@@ -1,20 +1,10 @@
 import * as Winston from 'winston';
-import { Signer, providers } from 'ethers';
 
 import { IOffChainDataSource } from '@energyweb/origin-backend-core';
 import { IDeviceTypeService } from './DeviceTypeService';
 
-export interface Entity<TRegistry = any, TIssuer = any> {
-    blockchainProperties: BlockchainProperties<TRegistry, TIssuer>;
+export interface Entity {
     logger: Winston.Logger;
     deviceTypeService?: IDeviceTypeService;
     offChainDataSource?: IOffChainDataSource;
-}
-
-export interface BlockchainProperties<TRegistry = any, TIssuer = any> {
-    web3?: providers.FallbackProvider | providers.JsonRpcProvider;
-    registry?: TRegistry;
-    issuer?: TIssuer;
-    activeUser?: Signer;
-    privateKey?: string;
 }
