a58c270aecfe782e7dcfa2d1da7f1dd2512624b2
Joseph Bagaric <bagaricjos@gmail.com>
2020-11-13 10:37:22 +0100

    feat(issuer-api): expose BlockchainProperties settings

---
+++ packages/issuer-api/src/pods/blockchain/blockchain-properties.controller.ts
@@ -0,0 +1,23 @@
+import { ActiveUserGuard } from '@energyweb/origin-backend-utils';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { BlockchainPropertiesDTO } from './blockchain-properties.dto';
+import { BlockchainPropertiesService } from './blockchain-properties.service';
+
+@ApiTags('blockchain-properties')
+@Controller('blockchain-properties')
+export class BlockchainPropertiesController {
+    constructor(private readonly blockchainPropertiesService: BlockchainPropertiesService) {}
+
+    @Get()
+    @UseGuards(AuthGuard(), ActiveUserGuard)
+    @ApiResponse({
+        status: 200,
+        type: BlockchainPropertiesDTO,
+        description: 'Returns blockchain properties'
+    })
+    public async get(): Promise<BlockchainPropertiesDTO> {
+        return this.blockchainPropertiesService.dto();
+    }
+}
