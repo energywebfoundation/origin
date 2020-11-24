88bcaa9de80e1d7ec857446986136cf156ffdb3f
JosephBagaric <bagaricjos@gmail.com>
2020-11-24 11:46:45 +0100

    chore: update migrations

--- packages/origin-backend/migrations/1606214214687-UpdateTables.ts
+++ packages/origin-backend/migrations/1606214214687-UpdateTables.ts
@@ -1,9 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class SwaggerChanges1604562378046 implements MigrationInterface {
-    name = 'SwaggerChanges1604562378046';
+export class UpdateTables1606214214687 implements MigrationInterface {
+    name = 'UpdateTables1606214214687';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`COMMENT ON COLUMN "configuration"."id" IS NULL`);
+        await queryRunner.query(`ALTER TABLE "configuration" ALTER COLUMN "id" SET DEFAULT 1`);
         await queryRunner.query(`ALTER TABLE "organization_invitation" DROP COLUMN "status"`);
         await queryRunner.query(
             `ALTER TABLE "organization_invitation" ADD "status" character varying NOT NULL`
@@ -15,5 +17,7 @@ export class SwaggerChanges1604562378046 implements MigrationInterface {
         await queryRunner.query(
             `ALTER TABLE "organization_invitation" ADD "status" integer NOT NULL`
         );
+        await queryRunner.query(`ALTER TABLE "configuration" ALTER COLUMN "id" SET DEFAULT '1'`);
+        await queryRunner.query(`COMMENT ON COLUMN "configuration"."id" IS NULL`);
     }
 }
