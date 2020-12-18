e7a860da20028e90b47bd429405ad78a569ecb23
Alexander Popadyuk <alexander.popadyuk@482.solutions>
2020-12-18 10:28:04 +0200

    chore(exchange-ui-core): exchange state naming

--- packages/exchange-ui-core/src/features/general/selectors.ts
+++ packages/exchange-ui-core/src/features/general/selectors.ts
@@ -1,9 +1,9 @@
 import { ExchangeClient } from '../../utils/exchange';
-import { IStoreState } from '../../types';
+import { IExchangeState } from '../../types';
 import { IEnvironment } from './actions';
 
-export const getEnvironment = (state: IStoreState): IEnvironment =>
+export const getEnvironment = (state: IExchangeState): IEnvironment =>
     state.exchangeGeneralState.environment;
 
-export const getExchangeClient = (state: IStoreState): ExchangeClient =>
+export const getExchangeClient = (state: IExchangeState): ExchangeClient =>
     state.exchangeGeneralState.exchangeClient;
