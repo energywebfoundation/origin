1820dc2eb818153f67b4dfa870fc2b4fe874c385
arvindchez <arvind.chez@gmail.com>
2021-01-28 16:47:03 +0530

    Moved discovered handler to transfer pod

--- packages/exchange/src/pods/post-for-sale/handlers/index.ts
+++ packages/exchange/src/pods/post-for-sale/handlers/index.ts
@@ -1,2 +1,1 @@
 export * from './deposit-approved-event.handler';
-export * from './deposit-discovered-event.handler';
--- packages/exchange/src/pods/transfer/transfer.module.ts
+++ packages/exchange/src/pods/transfer/transfer.module.ts
@@ -4,12 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AccountModule } from '../account/account.module';
 import { AssetModule } from '../asset/asset.module';
+import { DepositDiscoveredEventHandler } from './handlers';
 import { TransferController } from './transfer.controller';
 import { Transfer } from './transfer.entity';
 import { TransferService } from './transfer.service';
 
 @Module({
-    providers: [TransferService],
+    providers: [TransferService, DepositDiscoveredEventHandler],
     imports: [TypeOrmModule.forFeature([Transfer]), AssetModule, CqrsModule, AccountModule],
     exports: [TransferService],
     controllers: [TransferController]
