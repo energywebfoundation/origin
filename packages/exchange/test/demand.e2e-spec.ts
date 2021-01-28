82915d4469fb13f81b2aa74789a5d05d9d144781
arvindchez <arvind.chez@gmail.com>
2021-01-29 04:45:20 +0530

    chore(exchange-io-erc1888): Move supply watching functionality from erc1888 watcher to exchange using event modelling

--- packages/exchange/test/demand.e2e-spec.ts
+++ packages/exchange/test/demand.e2e-spec.ts
@@ -40,11 +40,17 @@ const dummyAsset = {
 
 const transactionHash = `0x${((Math.random() * 0xffffff) << 0).toString(16)}`;
 
-const createDeposit = (address: string, amount = `${1000 * MWh}`, asset = dummyAsset) => {
+const createDeposit = (
+    address: string,
+    amount = `${1000 * MWh}`,
+    asset = dummyAsset,
+    blockNumber = 123456
+) => {
     return transferService.createDeposit({
         address,
         transactionHash,
         amount,
+        blockNumber,
         asset
     });
 };
