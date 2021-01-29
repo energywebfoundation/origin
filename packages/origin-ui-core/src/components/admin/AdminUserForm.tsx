1fecdd23868d9f3476085ce7a3f527389f1d4c4f
Joseph Bagaric <bagaricjos@gmail.com>
2021-01-29 14:48:44 +0100

    chore(origin-ui-core): remove unused code and streamline verifying blockchain account

--- packages/origin-ui-core/src/components/admin/AdminUserForm.tsx
+++ packages/origin-ui-core/src/components/admin/AdminUserForm.tsx
@@ -1,4 +1,4 @@
-import { IUser, UserUpdateData, UserStatus, KYCStatus } from '@energyweb/origin-backend-core';
+import { IUser, UserStatus, KYCStatus } from '@energyweb/origin-backend-core';
 import { Button, createStyles, Grid, makeStyles, Paper, useTheme } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import { Form, Formik, FormikHelpers } from 'formik';
@@ -77,11 +77,9 @@ export function AdminUserForm(props: IProps) {
         dispatch(setLoading(true));
 
         try {
-            const formData: UserUpdateData = {
-                ...values
-            };
-
-            await adminClient.updateUser(values.id.toString(), formData);
+            await adminClient.updateUser(values.id, {
+                notifications: values.notifications
+            });
 
             history.push('manage-user');
 
