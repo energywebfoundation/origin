02d049d1266e1e6137d83d0691474e88fc7e6349
Alexander Popadyuk <alexander.popadyuk@482.solutions>
2021-02-01 18:14:40 +0200

    feat(origin-ui): connect irec-ui and hide under feature flag

--- packages/origin-ui/src/types/index.ts
+++ packages/origin-ui/src/types/index.ts
@@ -1,4 +1,5 @@
 import { ICoreState } from '@energyweb/origin-ui-core';
 import { IExchangeState } from '@energyweb/exchange-ui-core';
+import { IIRecAppState } from '@energyweb/origin-ui-irec-core';
 
-export interface IStoreState extends ICoreState, IExchangeState {}
+export interface IStoreState extends ICoreState, IExchangeState, IIRecAppState {}
