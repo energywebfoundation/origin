121fafd84515d482b42b477fe9f8c617481d22b2
Piotr Kosiński <piotrkosinski@gmail.com>
2021-02-03 13:44:05 +0100

    chore: package reorg

---
+++ packages/trade/exchange-token-account/truffle-config.js
@@ -0,0 +1,33 @@
+require('ts-node/register');
+
+module.exports = {
+    test_file_extension_regexp: /.*\.ts$/,
+
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8570,
+            network_id: '*',
+            gas: 8000000,
+            gasPrice: 0
+        },
+        coverage: {
+            host: 'localhost',
+            network_id: '*',
+            port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+            gas: 80000000, // <-- Use this high gas value
+            gasPrice: 0x01 // <-- Use this low gas price
+        }
+    },
+    compilers: {
+        solc: {
+            version: '../../../node_modules/solc',
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }
+    }
+};
---
+++ packages/ui/origin-ui-core/src/__tests__/unit/ProducingDeviceTable.test.tsx
@@ -0,0 +1,133 @@
+import { Configuration } from '@energyweb/device-registry';
+import { DeviceStatus } from '@energyweb/origin-backend-core';
+import { DeviceTypeService } from '@energyweb/utils-general';
+import { mount } from 'enzyme';
+import { BigNumber } from 'ethers';
+import React from 'react';
+
+import { ProducingDeviceTable } from '../../components/devices/ProducingDevice/ProducingDeviceTable';
+import { configurationUpdated } from '../../features';
+import { BackendClient, dataTestSelector } from '../../utils';
+import {
+    createRenderedHelpers,
+    setupStore,
+    TEST_DEVICE_TYPES,
+    WrapperComponent
+} from '../utils/helpers';
+
+describe('ProducingDeviceTable', () => {
+    it('correctly renders and search works', async () => {
+        const { store, history, addProducingDevice } = setupStore(undefined, {
+            backendClient: new BackendClient(
+                `${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}`
+            ),
+            mockUserFetcher: false,
+            logActions: false
+        });
+
+        const testOrganizationName = 'Test organization name';
+
+        addProducingDevice({
+            id: 0,
+            status: DeviceStatus.Active,
+            meterStats: {
+                uncertified: BigNumber.from(7777),
+                certified: BigNumber.from(0)
+            },
+            organizationId: 0
+        });
+
+        addProducingDevice({
+            id: 1,
+            status: DeviceStatus.Active,
+            facilityName: 'Biomass Energy Facility',
+            deviceType: 'Gaseous;Agricultural gas',
+            address:
+                '95 Moo 7, Sa Si Mum Sub-district, Kamphaeng Saen District, Nakhon Province 73140',
+            country: 'Thailand',
+            capacityInW: 736123,
+            region: 'Central',
+            province: 'Nakhon Pathom',
+            organizationId: 1
+        });
+
+        store.dispatch(
+            configurationUpdated(({
+                deviceTypeService: new DeviceTypeService(TEST_DEVICE_TYPES)
+            } as Partial<Configuration.Entity>) as Configuration.Entity)
+        );
+
+        const rendered = mount(
+            <WrapperComponent store={store} history={history}>
+                <ProducingDeviceTable
+                    hiddenColumns={['status']}
+                    actions={{
+                        requestCertificates: true
+                    }}
+                />
+            </WrapperComponent>
+        );
+
+        const { refresh, assertPagination, assertMainTableContent } = createRenderedHelpers(
+            rendered
+        );
+
+        await refresh();
+
+        assertMainTableContent([
+            testOrganizationName,
+            'Wuthering Heights facility',
+            'Solar - Photovoltaic - Roof mounted',
+            '9.877',
+            '0',
+            '0.777',
+            'View details',
+            // next device
+            testOrganizationName,
+            'Biomass Energy Facility',
+            'Gaseous - Agricultural gas',
+            '0.736',
+            '0',
+            '0',
+            'View details'
+        ]);
+
+        assertPagination(1, 2, 2);
+
+        const searchInput = rendered.find(
+            `${dataTestSelector(`Search by facility name and organization-textfield`)} input`
+        );
+
+        searchInput.simulate('change', { target: { value: 'Biomass' } });
+
+        await refresh();
+
+        assertMainTableContent([
+            testOrganizationName,
+            'Biomass Energy Facility',
+            'Gaseous - Agricultural gas',
+            '0.736',
+            '0',
+            '0',
+            'View details'
+        ]);
+
+        assertPagination(1, 1, 1);
+
+        searchInput.simulate('change', { target: { value: 'Wuthering Heights' } });
+
+        await refresh();
+
+        assertMainTableContent([
+            testOrganizationName,
+            'Wuthering Heights facility',
+            'Solar - Photovoltaic - Roof mounted',
+            '9.877',
+            '0',
+            '0.777',
+            'View details'
+        ]);
+
+        assertPagination(1, 1, 1);
+    });
+});
