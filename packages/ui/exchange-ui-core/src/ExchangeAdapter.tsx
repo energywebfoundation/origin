88148286eba3ad0ad81b22d742b94ecd7cc68fa3
Alexander Popadyuk <alexander.popadyuk@482.solutions>
2021-02-18 14:26:40 +0200

    chore(exchange-ui-core): remove i18next provider

--- packages/ui/exchange-ui-core/src/ExchangeAdapter.tsx
+++ packages/ui/exchange-ui-core/src/ExchangeAdapter.tsx
@@ -5,8 +5,6 @@ import MomentUtils from '@date-io/moment';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { History } from 'history';
-import { I18nextProvider } from 'react-i18next';
-import { i18n } from 'i18next';
 import { OriginConfigurationProvider, IOriginConfiguration } from '@energyweb/origin-ui-core';
 import { Store, CombinedState, AnyAction } from 'redux';
 import { IExchangeState } from './types';
@@ -16,7 +14,6 @@ interface IProps {
     configuration: IOriginConfiguration;
     history: History;
     component: React.ReactElement;
-    i18nInstance: i18n;
 }
 
 export function ExchangeAdapter(props: IProps) {
@@ -31,9 +28,7 @@ export function ExchangeAdapter(props: IProps) {
                         >
                             <Provider store={props.store}>
                                 <ConnectedRouter history={props.history}>
-                                    <I18nextProvider i18n={props.i18nInstance}>
-                                        {props.component}
-                                    </I18nextProvider>
+                                    {props.component}
                                 </ConnectedRouter>
                             </Provider>
                         </MuiPickersUtilsProvider>
