3a949cb98a007e1ac22762e928ad3f08c43f103a
Alexander Popadyuk <alexander.popadyuk@482.solutions>
2021-03-02 14:01:44 +0200

    chore(origin-device-registry-api): lint fix

--- packages/devices/origin-device-registry-api/src/device-registry/dto/device.dto.ts
+++ packages/devices/origin-device-registry-api/src/device-registry/dto/device.dto.ts
@@ -1,6 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { NewDeviceDTO } from "./new-device.dto";
-import { IsNotEmpty, IsString, IsUUID } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { NewDeviceDTO } from './new-device.dto';
 
 export class DeviceDTO extends NewDeviceDTO {
     @ApiProperty({ type: String })
