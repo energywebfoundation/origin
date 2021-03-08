7ddb57e0640ed97459fa4b69349e8b1d713198e8
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-03-08 11:06:15 +0200

    fix(origin-ui-core): hide myDevices under requirements

--- packages/ui/origin-ui-core/src/components/devices/MyDevices.tsx
+++ packages/ui/origin-ui-core/src/components/devices/MyDevices.tsx
@@ -2,19 +2,26 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBackendClient } from '../../features/general';
 import { getMyDevices, fetchMyDevices } from '../../features/devices';
+import { usePermissions } from '../../utils/permissions';
 import { TableFallback } from '../Table';
+import { Requirements } from '../Layout';
 import { DeviceTable } from './Table/DeviceTable';
 
 export function MyDevices() {
     const dispatch = useDispatch();
     const myDevices = useSelector(getMyDevices);
     const deviceClient = useSelector(getBackendClient)?.deviceClient;
+    const { canAccessPage } = usePermissions();
 
     useEffect(() => {
         if (deviceClient) {
             dispatch(fetchMyDevices());
         }
     }, [deviceClient]);
+
+    if (!canAccessPage.value) {
+        return <Requirements />;
+    }
 
     if (myDevices === null) {
         return <TableFallback />;
