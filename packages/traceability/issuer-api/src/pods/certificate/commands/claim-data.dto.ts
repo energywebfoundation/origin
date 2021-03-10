049cee498dbc65e60871d00b3f8bf503c3123cf9
Piotr Kosiński <piotrkosinski@gmail.com>
2021-03-10 13:07:01 +0100

    chore(issuer-api): added string validation to claim data

--- packages/traceability/issuer-api/src/pods/certificate/commands/claim-data.dto.ts
+++ packages/traceability/issuer-api/src/pods/certificate/commands/claim-data.dto.ts
@@ -1,25 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsOptional } from 'class-validator';
+import { IsDateString, IsOptional, IsString } from 'class-validator';
 
 export class ClaimDataDTO {
     @ApiProperty({ type: String, required: false })
     @IsOptional()
+    @IsString()
     beneficiary?: string;
 
     @ApiProperty({ type: String, required: false })
     @IsOptional()
+    @IsString()
     address?: string;
 
     @ApiProperty({ type: String, required: false })
     @IsOptional()
+    @IsString()
     region?: string;
 
     @ApiProperty({ type: String, required: false })
     @IsOptional()
+    @IsString()
     zipCode?: string;
 
     @ApiProperty({ type: String, required: false })
     @IsOptional()
+    @IsString()
     countryCode?: string;
 
     @ApiProperty({ type: Date, required: false })
