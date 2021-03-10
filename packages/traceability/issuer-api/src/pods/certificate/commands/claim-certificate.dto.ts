048143006ec9d0748c0007e1d94b46f53ef0246d
Piotr Kosiński <piotrkosinski@gmail.com>
2021-03-10 13:07:01 +0100

    chore(issuer-api): added string validation to claim data

--- packages/traceability/issuer-api/src/pods/certificate/commands/claim-certificate.dto.ts
+++ packages/traceability/issuer-api/src/pods/certificate/commands/claim-certificate.dto.ts
@@ -1,13 +1,14 @@
 import { IntUnitsOfEnergy, PositiveBNStringValidator } from '@energyweb/origin-backend-utils';
 import { ApiProperty } from '@nestjs/swagger';
-import { Validate, ValidateIf, ValidateNested } from 'class-validator';
+import { IsOptional, Validate, ValidateIf, ValidateNested } from 'class-validator';
 
 import { ClaimDataDTO } from './claim-data.dto';
 
 export class ClaimCertificateDTO {
     @ApiProperty({ type: ClaimDataDTO })
+    @IsOptional()
     @ValidateNested()
-    claimData: ClaimDataDTO;
+    claimData?: ClaimDataDTO;
 
     @ApiProperty({ type: String, required: false })
     @ValidateIf((dto: ClaimCertificateDTO) => !!dto.amount)
