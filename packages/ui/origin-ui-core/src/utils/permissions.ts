e812a8e813d0ba354084f847495c98a3cd1003ba
marcin-l-tsh <marcin.labryga@tsh.io>
2021-03-16 09:10:38 +0100

    feat(origin-ui-core): use permissions extended, blockchain inbox requirements added

--- packages/ui/origin-ui-core/src/utils/permissions.ts
+++ packages/ui/origin-ui-core/src/utils/permissions.ts
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { OrganizationStatus, UserStatus } from '@energyweb/origin-backend-core';
-import { getUserOffchain, getExchangeDepositAddress } from '../features/users';
+import { getUserOffchain, getExchangeDepositAddress } from '../features';
 
 export interface IPermissionRule {
     label: string;
@@ -13,33 +13,56 @@ export interface IPermission {
     rules: IPermissionRule[];
 }
 
-export function usePermissions() {
+export enum Requirement {
+    IsLoggedIn,
+    IsActiveUser,
+    IsPartOfApprovedOrg,
+    HasExchangeDepositAddress,
+    HasUserBlockchainAddress
+}
+
+type Requirements = Requirement[];
+
+const DefaultRequirements: Requirements = [
+    Requirement.IsLoggedIn,
+    Requirement.IsActiveUser,
+    Requirement.IsPartOfApprovedOrg,
+    Requirement.HasExchangeDepositAddress
+];
+
+export function usePermissions(config = DefaultRequirements): { canAccessPage: IPermission } {
     const user = useSelector(getUserOffchain);
     const exchangeAddress = useSelector(getExchangeDepositAddress);
     const { t } = useTranslation();
 
+    const tests: Record<Requirement, IPermissionRule> = {
+        [Requirement.IsLoggedIn]: {
+            label: t('general.feedback.haveToBeLoggedInUser'),
+            passing: Boolean(user)
+        },
+        [Requirement.IsActiveUser]: {
+            label: t('general.feedback.hasToBeActiveUser'),
+            passing: user?.status === UserStatus.Active
+        },
+        [Requirement.IsPartOfApprovedOrg]: {
+            label: t('general.feedback.userHasToBePartOfApprovedOrganization'),
+            passing:
+                Boolean(user?.organization) &&
+                user?.organization?.status === OrganizationStatus.Active
+        },
+        [Requirement.HasExchangeDepositAddress]: {
+            label: t('general.feedback.organizationHasToHaveExchangeDeposit'),
+            passing: Boolean(exchangeAddress)
+        },
+        [Requirement.HasUserBlockchainAddress]: {
+            label: t('general.feedback.userHasToHaveBlockchainAccount'),
+            passing: Boolean(user.blockchainAccountAddress)
+        }
+    };
+
     const canAccessPage: IPermission = {
         value: false,
-        rules: [
-            {
-                label: t('general.feedback.haveToBeLoggedInUser'),
-                passing: Boolean(user)
-            },
-            {
-                label: t('general.feedback.hasToBeActiveUser'),
-                passing: user?.status === UserStatus.Active
-            },
-            {
-                label: t('general.feedback.userHasToBePartOfApprovedOrganization'),
-                passing:
-                    Boolean(user?.organization) &&
-                    user?.organization?.status === OrganizationStatus.Active
-            },
-            {
-                label: t('general.feedback.organizationHasToHaveExchangeDeposit'),
-                passing: Boolean(exchangeAddress)
-            }
-        ]
+        rules: config.map((r) => tests[r])
     };
 
     canAccessPage.value = canAccessPage.rules.every((r) => r.passing);
