21501ee23ba04cd9d9f398556de756ae7d91b78f
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-03-30 16:24:31 +0300

    chore(origin-ui-core): configure storybook

--- packages/ui/origin-ui-core/.storybook/main.js
+++ packages/ui/origin-ui-core/.storybook/main.js
@@ -1,35 +1,4 @@
-const path = require('path');
-
 module.exports = {
-    stories: ['../src/components/**/*.stories.tsx'],
-    addons: [
-        '@storybook/addon-knobs',
-        '@storybook/addon-storysource',
-        '@storybook/addon-backgrounds',
-        {
-            name: '@storybook/preset-typescript',
-            options: {
-                tsLoaderOptions: {
-                    configFile: path.resolve(__dirname, '../tsconfig.json'),
-                },
-                tsDocgenLoaderOptions: {
-                    tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-                },
-                forkTsCheckerWebpackPluginOptions: {
-                    colors: false, // disables built-in colors in logger messages
-                },
-                include: [path.resolve(__dirname, '../src')],
-            },
-        },
-        '@storybook/preset-scss',
-        '@storybook/addon-actions/register'
-    ],
-    webpackFinal: (config) => ({
-        ...config,
-        externals: [
-            {
-                xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
-            }
-        ]
-    })
-}
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+};
