d222bed3841c6968b752f2f0672efd5506ca99c7
Arkadiusz Szaleniec <arkadiusz.szaleniec@j-labs.pl>
2021-04-02 12:41:48 +0200

    chore(origin-ui) Fixed deepscan issues.

--- packages/ui/origin-ui-v2/apps/origin-ui/src/app/app.tsx
+++ packages/ui/origin-ui-v2/apps/origin-ui/src/app/app.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
-
 const StyledApp = styled.div`
   font-family: sans-serif;
 `;
