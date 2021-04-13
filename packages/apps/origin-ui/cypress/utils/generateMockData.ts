f0a8d57a06d26b8fb398e8f2e35c13ed3b91f14d
driamend <driamend@gmail.com>
2021-04-13 13:32:22 +0300

    chore(origin-ui): add testDevice fixture, device register commands, mock and types

--- packages/apps/origin-ui/cypress/utils/generateMockData.ts
+++ packages/apps/origin-ui/cypress/utils/generateMockData.ts
@@ -1,5 +1,6 @@
 import * as testUser from '../fixtures/testUser.json';
 import * as testOrg from '../fixtures/testOrganization.json';
+import * as testDevice from '../fixtures/testDevice.json';
 
 export const generateNewUser = (): UserRegisterData => {
     const randomNumber = Math.round(Math.random() * 100000);
@@ -19,5 +20,12 @@ export const generateNewOrg = (user: UserRegisterData): OrganizationPostData => 
         name: `${testOrg.name} ${userNumber}`,
         address: `${testOrg.address} ${userNumber}`,
         zipCode: `${testOrg.zipCode}${userNumber}`
+    };
+};
+
+export const generateNewDevice = (user: UserRegisterData): DevicePostData => {
+    return {
+        ...testDevice,
+        facilityName: `${user.firstName}-facility`
     };
 };
