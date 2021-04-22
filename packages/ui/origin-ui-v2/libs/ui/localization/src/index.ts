1b4b27deac7f771cb566933407bad6b9d4cb4ebd
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-04-22 17:23:01 +0300

    feat(origin-ui-localization): add translation files

--- packages/ui/origin-ui-v2/libs/ui/localization/src/index.ts
+++ packages/ui/origin-ui-v2/libs/ui/localization/src/index.ts
@@ -1,4 +1,13 @@
+import en from './languages/en.json';
+import pl from './languages/pl.json';
+
 export const enum SupportedLanguagesEnum {
   en = 'en',
   pl = 'pl',
 }
+
+export const ORIGIN_TRANSLATIONS = { en, pl } as const;
+
+export const AvailableOriginLanguages = Object.keys(ORIGIN_TRANSLATIONS);
+
+export type TOriginLanguage = keyof typeof ORIGIN_TRANSLATIONS;
