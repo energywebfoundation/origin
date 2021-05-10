0bb693ee57b371110a48be1fe8e7c0f3c60fd3dc
Joseph Bagaric <bagaricjos@gmail.com>
2021-05-10 12:55:52 +0200

    chore(issuer): rm volta from truffle-config

--- packages/traceability/issuer/truffle-config.js
+++ packages/traceability/issuer/truffle-config.js
@@ -1,21 +1,10 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-
 module.exports = {
     networks: {
         development: {
-            host: '127.0.0.1',
+            host: 'localhost',
             port: 8560,
             network_id: '*',
             gas: 5000000
-        },
-        volta: {
-            provider: function () {
-                return new HDWalletProvider(
-                    'chalk park staff buzz chair purchase wise oak receive avoid avoid home',
-                    'https://volta.rpc.anyblock.tools/'
-                );
-            },
-            network_id: 73799
         }
     },
     compilers: {
