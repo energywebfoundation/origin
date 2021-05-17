349fb37a12c8eb18a74193a730dc437364229dd4
Joseph Bagaric <bagaricjos@gmail.com>
2021-05-17 16:50:34 +0200

    chore: add support for certificate metadata

--- packages/traceability/issuer-api/src/pods/certificate/commands/issue-certificate.command.ts
+++ packages/traceability/issuer-api/src/pods/certificate/commands/issue-certificate.command.ts
@@ -6,6 +6,7 @@ export class IssueCertificateCommand {
         public readonly toTime: number,
         public readonly deviceId: string,
         public readonly userId: string,
-        public readonly isPrivate = false
+        public readonly isPrivate = false,
+        public readonly metadata = ''
     ) {}
 }
