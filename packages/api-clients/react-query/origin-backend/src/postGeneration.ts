2f2f60891816ac083eba47c64bdf4afc7dc41381
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-05-18 15:58:08 +0300

    chore(origin-backend-react-query-client): improve postGeneration script for file uploads

--- packages/api-clients/react-query/origin-backend/src/postGeneration.ts
+++ packages/api-clients/react-query/origin-backend/src/postGeneration.ts
@@ -2,13 +2,22 @@ import fs from 'fs';
 
 const file = fs.readFileSync('./src/client/file.ts', 'utf8');
 
-const regularUploadReplaced = file.replace(
-    `formData.append('files', JSON.stringify(fileControllerUploadBody.files))`,
-    `formData.append('files', JSON.stringify(fileUploadDto.files))`
-);
-const bothUploadsReplaced = regularUploadReplaced.replace(
-    `formData.append('files', JSON.stringify(fileControllerUploadAnonymouslyBody.files))`,
-    `formData.append('files', JSON.stringify(fileUploadDto.files))`
-);
+const newFile = file
+    .replace(
+        `formData.append('files', JSON.stringify(fileControllerUploadBody.files))`,
+        `if (fileUploadDto.files) {
+        fileUploadDto.files.forEach((element) => {
+            formData.append('files', element as any);
+        })
+    }`
+    )
+    .replace(
+        `formData.append('files', JSON.stringify(fileControllerUploadAnonymouslyBody.files))`,
+        `if (fileUploadDto.files) {
+        fileUploadDto.files.forEach((element) => {
+            formData.append('files', element as any);
+        })
+    }`
+    );
 
-fs.writeFileSync('./src/client/file.ts', bothUploadsReplaced);
+fs.writeFileSync('./src/client/file.ts', newFile);
