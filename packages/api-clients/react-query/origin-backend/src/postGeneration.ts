f8c7ab41052dab9bc89821fac4024d896351bde0
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-05-20 17:51:47 +0300

    fix(origin-backend-react-query-client): adjust file upload to custom response type

--- packages/api-clients/react-query/origin-backend/src/postGeneration.ts
+++ packages/api-clients/react-query/origin-backend/src/postGeneration.ts
@@ -4,20 +4,30 @@ const file = fs.readFileSync('./src/client/file.ts', 'utf8');
 
 const newFile = file
     .replace(
-        `formData.append('files', JSON.stringify(fileControllerUploadBody.files))`,
-        `if (fileUploadDto.files) {
-        fileUploadDto.files.forEach((element) => {
-            formData.append('files', element as any);
-        })
-    }`
+        "      return fileControllerUploadMutator<Data extends unknown ? string[] : Data>({url: `/api/file`, method: 'post',",
+        `
+    const formData = new FormData();
+
+    if (fileUploadDto.files) {
+    fileUploadDto.files.forEach((element) => {
+        formData.append('files', element as any);
+    })
+    }
+    return fileControllerUploadMutator<Data extends unknown ? string[] : Data>({url: '/api/file', method: 'post',`
     )
     .replace(
-        `formData.append('files', JSON.stringify(fileControllerUploadAnonymouslyBody.files))`,
-        `if (fileUploadDto.files) {
-        fileUploadDto.files.forEach((element) => {
-            formData.append('files', element as any);
-        })
-    }`
-    );
+        "      return fileControllerUploadAnonymouslyMutator<Data extends unknown ? string[] : Data>({url: `/api/file/public`, method: 'post',",
+        `
+    const formData = new FormData();
+
+    if (fileUploadDto.files) {
+    fileUploadDto.files.forEach((element) => {
+        formData.append('files', element as any);
+    })
+    }
+    return fileControllerUploadAnonymouslyMutator<Data extends unknown ? string[] : Data>({url: '/api/file/public', method: 'post',`
+    )
+    .replace(`      data: fileUploadDto`, `      data: formData`)
+    .replace(`      data: fileUploadDto`, `      data: formData`);
 
 fs.writeFileSync('./src/client/file.ts', newFile);
