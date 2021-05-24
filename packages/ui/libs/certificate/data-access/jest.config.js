01775844372d6cae47d29379e7e7c73daeb11968
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-05-24 12:12:12 +0300

    chore: packages clean-up

--- packages/ui/libs/certificate/data-access/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'certificate-data-access',
-  preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/certificate/data-access',
-};
--- packages/ui/libs/exchange/state-management/README.md
+++
@@ -1,7 +0,0 @@
-# exchange-state-management
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test exchange-state-management` to execute the unit tests via [Jest](https://jestjs.io).
