1ab76ce67d2292532dc997cc680c7812c18e1b06
Aleksandr Marenin <comrade.muchacho@gmail.com>
2021-05-26 12:54:28 +0400

    feat(origin-device-registry-irec-local-api): added postalCode, country, region, subregion fields to irec devices

--- packages/ui/origin-ui-irec-core/src/types/CreateDevice.ts
+++ packages/ui/origin-ui-irec-core/src/types/CreateDevice.ts
@@ -20,4 +20,8 @@ export type CreateDeviceData = {
     description: string;
     externalDeviceIds?: IExternalDeviceId[];
     imageIds: string[];
+    country: string;
+    postalCode: string;
+    region: string;
+    subregion: string;
 };
