42237994a40427a7243a685b3ecc8b525bc7a5bb
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-06-16 10:00:35 +0300

    chore(origin-ui-device-data): add comments for missing/mocked fields in device creation request

--- packages/ui/libs/device/data/src/utils.ts
+++ packages/ui/libs/device/data/src/utils.ts
@@ -59,9 +59,11 @@ export function decomposeForIRec(
 ): IRecCreateDeviceDTO {
   const iRecCreateDevice: IRecCreateDeviceDTO = {
     name: newDevice.facilityName,
+    // should it be the blockchainAccount or exchangeDepositAddress?
     defaultAccount: organization.blockchainAccountAddress,
     deviceType: newDevice.deviceType,
     fuelType: newDevice.fuelType,
+    // should it the organization country or user should add this in device form registration?
     countryCode: organization.country,
     capacity: Number(newDevice.capacity),
     commissioningDate: newDevice.commissioningDate,
@@ -69,10 +71,14 @@ export function decomposeForIRec(
     address: newDevice.address,
     latitude: newDevice.latitude,
     longitude: newDevice.longitude,
+    // is notes simillar to project story from old device api?
     notes: newDevice.projectStory,
+    // should we use an env variable for propery (e.g. MARKET_TIMEZONE) or this will be taken based on user locale?
     timezone: 'Asia/Bangkok',
     gridOperator: newDevice.gridOperator,
+    // should it be a separate field or taken from organization?
     postalCode: organization.zipCode,
+    // should it be a separate field or taken from organization?
     country: organization.country,
     region: newDevice.region,
     subregion: newDevice.subregion,
@@ -87,7 +93,9 @@ export function decomposeForOrigin(
   return {
     externalRegistryId: null,
     smartMeterId: newDevice.smartMeterId,
+    // should it be the same as notes in i-rec device?
     description: newDevice.projectStory,
+    // isn't it similar to smartMeterId? This is mocked for now
     externalDeviceIds: [
       { id: '123', type: 'Smart Meter Readings API ID' },
       { id: 'ABQ123-1', type: 'Issuer ID' },
