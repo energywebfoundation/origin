26fadcb86af7f407a2c8837ccd5686ce9571e09f
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-07-08 17:10:20 +0300

    chore(origin-ui-certificate-view): mock blockchain inbox actions

--- packages/ui/libs/certificate/view/src/containers/action/RetireAction/RetireAction.tsx
+++ packages/ui/libs/certificate/view/src/containers/action/RetireAction/RetireAction.tsx
@@ -2,10 +2,5 @@ import React, { FC } from 'react';
 import { CertificateActionContent } from '../../list';
 
 export const RetireAction: FC = () => {
-  return (
-    <CertificateActionContent
-      title="Selected for Retirement"
-      buttonText="Retire"
-    />
-  );
+  return <div>Retire action</div>;
 };
