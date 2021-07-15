674085ebf466210234ddf4ac28bed65498c15aed
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-07-15 17:19:58 +0300

    feat(origin-ui): configure showing invitations tab

--- packages/ui/apps/origin-ui/src/AppContainer/AppContainer.tsx
+++ packages/ui/apps/origin-ui/src/AppContainer/AppContainer.tsx
@@ -3,11 +3,8 @@ import React from 'react';
 import { App, OriginGlobalStyles } from '../components';
 import { useAppContainerEffects } from './AppContainer.effects';
 import { NotificationsCenter } from '@energyweb/origin-ui-core';
-import { useAxiosInterceptors } from '@energyweb/origin-ui-react-query-providers';
 
 export const AppContainer = () => {
-  useAxiosInterceptors();
-
   const { topbarButtons, menuSections, user, isAuthenticated } =
     useAppContainerEffects();
 
