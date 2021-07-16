ffc7185045d1ee0ecb2094d7515d902340cfc464
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-07-16 10:17:43 +0300

    fix(origin-ui-certificate): separate logic hook for pending page

---
+++ packages/ui/libs/certificate/logic/src/pending/pending.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  PowerFormatter,
+  EnergyFormatter,
+  formatDate,
+} from '@energyweb/origin-ui-utils';
+import { downloadFileHandler } from '@energyweb/origin-ui-certificate-data';
+import {
+  TFormatPendingCertificatesData,
+  TUsePendingCertificatesLogic,
+} from './types';
+
+const formatRequestsData: TFormatPendingCertificatesData = ({
+  devices,
+  requests,
+  actions,
+  allFuelTypes,
+}) => {
+  const { t } = useTranslation();
+
+  return requests.length > 0 && devices.length > 0
+    ? requests.map((request) => {
+        const status = request?.approved
+          ? t('certificate.requests.approved')
+          : t('certificate.requests.pending');
+        const device = devices.find(
+          (device) => request?.deviceId === device.externalRegistryId
+        );
+
+        return {
+          id: request?.id,
+          facility: device?.name,
+          location: `${device?.region}, ${device?.subregion}`,
+          gridOperator: device?.gridOperator,
+          fuelType:
+            allFuelTypes?.find((type) => type.code === device?.fuelType)
+              ?.name || '',
+          capacity: PowerFormatter.format(device?.capacity),
+          meterRead: EnergyFormatter.format(request?.energy),
+          files: request?.files?.map((fileId) => (
+            <div key={fileId}>
+              <a
+                style={{ cursor: 'pointer', textDecoration: 'underline' }}
+                onClick={() => {
+                  downloadFileHandler(fileId, fileId);
+                }}
+              >
+                {fileId}
+              </a>
+            </div>
+          )),
+          timeFrame: (
+            <div style={{ whiteSpace: 'nowrap' }}>
+              {formatDate(request?.fromTime * 1000)} -{' '}
+              {formatDate(request?.toTime * 1000)}
+            </div>
+          ),
+          status,
+          actions,
+        };
+      })
+    : [];
+};
+
+export const usePendingCertificatsLogic: TUsePendingCertificatesLogic = ({
+  devices,
+  requests,
+  allFuelTypes,
+  actions,
+  loading,
+}) => {
+  const { t } = useTranslation();
+  return {
+    header: {
+      facility: t('certificate.requests.facility'),
+      location: t('certificate.requests.location'),
+      gridOperator: t('certificate.requests.gridOperator'),
+      fuelType: t('certificate.requests.fuelType'),
+      capacity: `${t('certificate.requests.capacity')} (${
+        PowerFormatter.displayUnit
+      })`,
+      meterRead: `${t('certificate.requests.meterRead')} (${
+        EnergyFormatter.displayUnit
+      })`,
+      files: t('certificate.requests.files'),
+      timeFrame: t('certificate.requests.timeFrame'),
+      status: t('certificate.requests.status'),
+      actions: '',
+    },
+    pageSize: 10,
+    loading: loading,
+    data: formatRequestsData({
+      devices,
+      requests,
+      actions,
+      allFuelTypes,
+    }),
+  };
+};
