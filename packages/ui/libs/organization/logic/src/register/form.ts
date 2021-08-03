78452eb634ddced5ceec50232203f17126a4d17e
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-08-03 15:41:09 +0300

    chore(origin-ui-organization-logic): adjust multi step forms to changes

--- packages/ui/libs/organization/logic/src/register/form.ts
+++ packages/ui/libs/organization/logic/src/register/form.ts
@@ -4,14 +4,16 @@ import { createSignatoryInfoForm } from './signatoryInfoForm';
 import { createDocsUploadForm } from './docsUpload';
 import { useTranslation } from 'react-i18next';
 
-export const useRegisterOrganizationFormLogic: TUseRegisterOrganizationFormLogic = () => {
-  const { t } = useTranslation();
-  return {
-    heading: t('organization.register.formTitle'),
-    forms: [
-      createOrgInfoForm(t),
-      createSignatoryInfoForm(t),
-      createDocsUploadForm(t),
-    ],
+export const useRegisterOrganizationFormLogic: TUseRegisterOrganizationFormLogic =
+  () => {
+    const { t } = useTranslation();
+    return {
+      heading: t('organization.register.formTitle'),
+      forms: [
+        createOrgInfoForm(t),
+        createSignatoryInfoForm(t),
+        createDocsUploadForm(t),
+      ],
+      backButtonText: t('general.buttons.back'),
+    };
   };
-};
