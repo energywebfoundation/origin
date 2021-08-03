18b5ffb7a6914a51a0c3aabea10da9c90c7d5257
driamend <driamend@gmail.com>
2021-08-03 13:29:26 +0300

    chore(origin-ui): style browser-autocomplete for inputs

--- packages/ui/libs/ui/theme/src/utils/makeOriginUiConfig.ts
+++ packages/ui/libs/ui/theme/src/utils/makeOriginUiConfig.ts
@@ -10,6 +10,7 @@ export interface IOriginStyleConfig {
   PRIMARY_COLOR_DIM: string;
   TEXT_COLOR_DEFAULT: string;
   SIMPLE_TEXT_COLOR: string;
+  INPUT_AUTOFILL_COLOR: string;
   MAIN_BACKGROUND_COLOR: string;
   FIELD_ICON_COLOR: string;
   WHITE: string;
@@ -35,6 +36,7 @@ export function createStyleConfig(
     PRIMARY_COLOR_DIM: themeVariables.primaryColorDim ?? DEFAULT_COLOR,
     TEXT_COLOR_DEFAULT: themeVariables.textColorDefault ?? DEFAULT_COLOR,
     SIMPLE_TEXT_COLOR: themeVariables.simpleTextColor ?? DEFAULT_COLOR,
+    INPUT_AUTOFILL_COLOR: themeVariables.inputAutofillColor ?? DEFAULT_COLOR,
     MAIN_BACKGROUND_COLOR: themeVariables.mainBackgroundColor ?? DEFAULT_COLOR,
     FIELD_ICON_COLOR: themeVariables.fieldIconColor ?? DEFAULT_COLOR,
     WHITE: 'rgb(255,255,255)',
