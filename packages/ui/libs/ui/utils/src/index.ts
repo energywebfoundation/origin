3f19e966fc659be5e7d86eb4d4c6f963c9f90f46
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-08-06 08:50:32 +0300

    chore: move initializeI18 to localization package from utils

--- packages/ui/libs/ui/utils/src/index.ts
+++ packages/ui/libs/ui/utils/src/index.ts
@@ -2,6 +2,5 @@ export * from './countries';
 export * from './date';
 export * from './formatters';
 export * from './permissions';
-export * from './localization';
 export * from './types';
 export * from './checks';
