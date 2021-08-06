8da5eb6a092fcf9ae00e856f6b7dd473eba07f88
driamend <driamend@gmail.com>
2021-08-06 16:36:51 +0300

    chore(origin-ui): remove permissions and requirement types

--- packages/ui/libs/ui/utils/src/types/index.ts
+++ packages/ui/libs/ui/utils/src/types/index.ts
@@ -1,5 +1,3 @@
-import { UserDTO } from '@energyweb/origin-backend-react-query-client';
-
 export enum EnergyTypeEnum {
   WIND = 'wind',
   SOLAR = 'solar',
@@ -14,32 +12,3 @@ export enum EnergyTypeEnum {
   BIOGAS = 'biogas',
   CO_FIRED_WITH_FOSSIL = 'co-fired with fossil',
 }
-
-export interface IPermissionRule {
-  label: string;
-  passing: boolean;
-}
-
-export interface TUsePermissions {
-  user: UserDTO;
-  exchangeDepositAddress: string;
-  loading: boolean;
-  config?: Requirement[];
-}
-
-export interface IPermissionReturnType {
-  canAccessPage: boolean;
-  loading: boolean;
-  accessRules: IPermissionRule[];
-  title: string;
-}
-
-export enum Requirement {
-  IsLoggedIn,
-  IsActiveUser,
-  IsPartOfApprovedOrg,
-  HasExchangeDepositAddress,
-  HasOrganizationBlockchainAddress,
-}
-
-export type RequirementList = Requirement[];
