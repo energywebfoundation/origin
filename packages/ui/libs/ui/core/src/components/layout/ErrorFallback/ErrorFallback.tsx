a197af1086cb4d34278fe3ecac362e9230558854
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-08-09 10:18:08 +0300

    feat(origin-ui-core): make error fallback more customizable

--- packages/ui/libs/ui/core/src/components/layout/ErrorFallback/ErrorFallback.tsx
+++ packages/ui/libs/ui/core/src/components/layout/ErrorFallback/ErrorFallback.tsx
@@ -1,20 +1,49 @@
-import React, { FC } from 'react';
+import {
+  Paper,
+  PaperProps,
+  Typography,
+  TypographyProps,
+} from '@material-ui/core';
+import { Global, css } from '@emotion/react';
+import React, { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import { useStyles } from './ErrorFallback.styles';
 
 interface ErrorFallbackProps {
   error: Error;
+  title?: string;
+  wrapperProps?: DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >;
+  paperProps?: PaperProps;
+  titleProps?: TypographyProps;
+  messageProps?: TypographyProps;
 }
 
-export const ErrorFallback: FC<ErrorFallbackProps> = ({ error }) => {
-  const handleReloadClick = () => {
-    window.location.reload();
-  };
-
+export const ErrorFallback: FC<ErrorFallbackProps> = ({
+  title,
+  error,
+  wrapperProps,
+  paperProps,
+  titleProps,
+  messageProps,
+  children,
+}) => {
+  const classes = useStyles();
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <br />
-      <button onClick={handleReloadClick}>Reload</button>
+    <div className={classes.wrapper} {...wrapperProps}>
+      <Paper className={classes.paper} {...paperProps}>
+        {title && (
+          <Typography variant="h5" align="center" gutterBottom {...titleProps}>
+            {title}
+          </Typography>
+        )}
+        <Typography color="textSecondary" align="center" {...messageProps}>
+          {error.message}
+        </Typography>
+        {children}
+      </Paper>
+      <Global styles={css({ body: { margin: 0 } })} />
     </div>
   );
 };
