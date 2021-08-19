ac39cd270ae86254b52dc53a0b45e80c2d47601c
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-08-19 17:00:01 +0300

    chore(origin-ui-theme): added export of ui-config creation func

--- packages/ui/libs/ui/theme/src/index.ts
+++ packages/ui/libs/ui/theme/src/index.ts
@@ -1,3 +1,4 @@
 export * from './theme-provider/OriginThemeProvider';
 export * from './config/variables';
 export * from './utils/colors';
+export * from './utils/makeOriginUiConfig';
--- packages/ui/libs/ui/theme/src/theme-provider/OriginThemeProvider.tsx
+++ packages/ui/libs/ui/theme/src/theme-provider/OriginThemeProvider.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
-import makeOriginUiConfig from '../utils/makeOriginUiConfig';
+import { makeOriginUiConfig } from '../utils/makeOriginUiConfig';
 
 export const OriginThemeProvider: FC = ({ children }) => {
   const configuration = makeOriginUiConfig();
