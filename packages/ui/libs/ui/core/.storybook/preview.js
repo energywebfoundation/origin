ecad94f14ef4f72bbf8a51f218ac2d3e874f49d2
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-08-31 07:19:34 +0300

    docs(origin-ui-core): add storybook docs for GenericForm

--- packages/ui/libs/ui/core/.storybook/preview.js
+++ packages/ui/libs/ui/core/.storybook/preview.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import { MemoryRouter } from 'react-router';
-import {
-  OriginThemeProvider,
-  makeOriginUiConfig,
-} from '@energyweb/origin-ui-theme';
+import { makeOriginUiConfig } from '@energyweb/origin-ui-theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
 import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
 
 const uiConfiguration = makeOriginUiConfig();
+export const OriginThemeProvider = ({ children }) => {
+  const configuration = makeOriginUiConfig();
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={configuration.materialTheme}>
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
 
 const themeDecorator = (Story, context) => (
   <EmotionThemeProvider theme={uiConfiguration.materialTheme}>
