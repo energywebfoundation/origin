0c60b63b39105f450a1d5f087f70abd2a3a36ee2
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-09-01 09:37:44 +0300

    feat(origin-ui-core): extend CardWithImage and related components props to allow more customization

--- packages/ui/libs/ui/core/src/components/images/ImageWithHoverText/ImageWithHoverText.tsx
+++ packages/ui/libs/ui/core/src/components/images/ImageWithHoverText/ImageWithHoverText.tsx
@@ -1,25 +1,47 @@
-import React, { FC, ImgHTMLAttributes } from 'react';
+import React, {
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes,
+  ImgHTMLAttributes,
+} from 'react';
 import { useStyles } from './ImageWithHoverText.styles';
 
 export interface ImageWithHoverTextProps {
   src: string;
   text: string;
   alt?: string;
+  imageWrapperProps?: DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >;
   imageProps?: ImgHTMLAttributes<HTMLImageElement>;
+  overlayProps?: DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >;
+  overlayTextProps?: DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >;
 }
 
 export const ImageWithHoverText: FC<ImageWithHoverTextProps> = ({
   src,
   text,
   alt,
+  imageWrapperProps,
   imageProps,
+  overlayProps,
+  overlayTextProps,
 }) => {
   const classes = useStyles();
   return (
-    <div {...imageProps}>
+    <div {...imageWrapperProps}>
       <img src={src} alt={alt} {...imageProps} />
-      <div className={classes.overlay}>
-        <div className={classes.text}>{text}</div>
+      <div className={classes.overlay} {...overlayProps}>
+        <div className={classes.text} {...overlayTextProps}>
+          {text}
+        </div>
       </div>
     </div>
   );
