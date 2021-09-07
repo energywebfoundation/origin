5150ae20eca88fa5d910dfb4e68480a524af32f0
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-09-07 16:52:08 +0300

    refactor(exchange-ui-core): remove package

--- packages/ui/exchange-ui-core/src/components/modal/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './BundleBought';
-export * from './OrderDetailsModal';
-export * from './RemoveOrderConfirmation';
-export * from './DemandUpdateModal';
-export * from './UpdateSupplyModal';
-export * from './RemoveSupplyConfirmation';
--- packages/ui/exchange-ui-core/src/features/bundles/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { IExchangeState } from '../../types';
-
-export const getBundles = (state: IExchangeState) => state.bundlesState.bundles;
-
-export const getShowBundleDetails = (state: IExchangeState) => state.bundlesState.showBundleDetails;
