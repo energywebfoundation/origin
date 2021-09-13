fe8fb2fc86a37544785052a922c4010d3175e24d
Alexander Popadyuk <alexander.popadyuk@energyweb.org>
2021-09-13 17:19:52 +0300

    feat(origin-ui-blockchain): now using connect-metamask block instead of auto connect on page enter

--- packages/ui/libs/ui/blockchain/src/hoc.tsx
+++ packages/ui/libs/ui/blockchain/src/hoc.tsx
@@ -1,41 +1,12 @@
-import React, { FC, useEffect } from 'react';
-import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
-import {
-  NotificationTypeEnum,
-  showNotification,
-} from '@energyweb/origin-ui-core';
-import { injectedConnector } from './connectors';
+import React, { FC } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { ConnectMetamask } from './components';
 
 export const withMetamask = (Component: FC<any>) => {
   const ComponentWithMetamask = (props: any) => {
-    const { activate } = useWeb3React();
+    const { account } = useWeb3React();
 
-    const noMetamaskInstalled = !(
-      (window as any).web3 || (window as any).ethereum
-    );
-
-    useEffect(() => {
-      if (noMetamaskInstalled) {
-        showNotification(
-          'This page requires Metamask to be installed',
-          NotificationTypeEnum.Warning
-        );
-      } else {
-        activate(injectedConnector, undefined, true).catch((error) => {
-          if (error instanceof UnsupportedChainIdError) {
-            showNotification(
-              `You are connected to the unsupported network`,
-              NotificationTypeEnum.Error
-            );
-          } else {
-            showNotification(
-              `Unknown error while connecting wallet. Please reload the page`,
-              NotificationTypeEnum.Error
-            );
-          }
-        });
-      }
-    }, []);
+    if (!account) return <ConnectMetamask />;
 
     return <Component {...props} />;
   };
