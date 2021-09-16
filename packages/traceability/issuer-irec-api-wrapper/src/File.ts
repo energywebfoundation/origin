c13842e118e9ad54702ba462d3167d31bb930ee5
Aleksandr Marenin <comrade.muchacho@gmail.com>
2021-09-16 17:01:43 +0400

    fix(issuer-irec-api-wrapper): fix create certification request with file

--- packages/traceability/issuer-irec-api-wrapper/src/File.ts
+++ packages/traceability/issuer-irec-api-wrapper/src/File.ts
@@ -5,6 +5,8 @@ export class FileIds {
     @IsOptional()
     @Expose({ name: 'file_data', toPlainOnly: true })
     @Transform((ids: string[]) => ids?.map((id) => ({ file_uid: id })) ?? [], { toPlainOnly: true })
-    @Transform((ids: any[]) => ids.map((val: any) => val?.file_uid ?? val), { toClassOnly: true })
+    @Transform((ids: any[]) => ids?.map((val: any) => val?.file_uid ?? val) ?? [], {
+        toClassOnly: true
+    })
     files?: string[];
 }
