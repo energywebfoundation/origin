95739f4645f9cd8dc8c8b06da6714a2e77b2731c
Joseph Bagaric <bagaricjos@gmail.com>
2021-10-11 14:31:24 +0200

    chore: Add SSL config to ormconfigs + further unified approach to ormconfigs

--- packages/utils/origin-backend-utils/src/db/connection.options.ts
+++ packages/utils/origin-backend-utils/src/db/connection.options.ts
@@ -1,6 +1,7 @@
 import { ConnectionOptions } from 'typeorm';
+import { ClientConfig } from 'pg';
 
-export const getDBConnectionOptions = (): ConnectionOptions => {
+export const getDBConnectionOptions = (): ConnectionOptions | ClientConfig => {
     if (process.env.DATABASE_URL) {
         const url = new URL(process.env.DATABASE_URL);
         return {
@@ -9,7 +10,8 @@ export const getDBConnectionOptions = (): ConnectionOptions => {
             port: parseInt(url.port, 10),
             username: url.username,
             password: url.password,
-            database: url.pathname.replace('/', '')
+            database: url.pathname.replace('/', ''),
+            ssl: Boolean(process.env.DB_SSL_OFF) ? false : { rejectUnauthorized: false }
         };
     }
 
