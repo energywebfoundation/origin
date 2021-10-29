b5bef636d6821309c008d84478d2fcae4ea970b4
Doga Oztuzun <unquale@foton.energy>
2021-10-29 11:09:24 +0300

    unused imported dependencies have been removed

--- packages/trade/exchange-irec/src/admin/admin-trade.controller.ts
+++ packages/trade/exchange-irec/src/admin/admin-trade.controller.ts
@@ -1,19 +1,11 @@
 import { BaseAdminTradeController } from '@energyweb/exchange';
-import {
-    UserDecorator,
-    ActiveUserGuard,
-    NullOrUndefinedResultInterceptor
-} from '@energyweb/origin-backend-utils';
+import { NullOrUndefinedResultInterceptor } from '@energyweb/origin-backend-utils';
 import {
     Controller,
-    Get,
-    HttpStatus,
-    UseGuards,
     UseInterceptors,
     UsePipes,
     ValidationPipe
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ProductDTO, ProductFilterDTO } from '../product';
 
