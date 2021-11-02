44e7c073813efacd810507f805ef46a263ffc55c
Aleksandr Marenin <comrade.muchacho@gmail.com>
2021-11-02 10:32:40 +0400

    chore(issuer-irec-api): support custom irec trade account codes on ui

--- packages/traceability/issuer-irec-api/src/pods/certification-request/create-irec-certification-request.dto.ts
+++ packages/traceability/issuer-irec-api/src/pods/certification-request/create-irec-certification-request.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateCertificationRequestDTO } from '@energyweb/issuer-api';
 
 export class CreateIrecCertificationRequestDTO extends CreateCertificationRequestDTO {
     @ApiProperty({ type: String })
     @IsString()
+    @IsOptional()
     irecTradeAccountCode?: string;
 }
