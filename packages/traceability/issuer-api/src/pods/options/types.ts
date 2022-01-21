fa9a72b678126cc8781228101074dbaf391a7682
Jonasz <jonaszwiacek@gmail.com>
2022-01-21 11:21:02 +0100

    feat: Remove transaction log from issuer-api (#3298)
    * feat: Remove transaction log from issuer-api
    * feat: Remove transaction history

--- packages/traceability/issuer-api/src/pods/options/types.ts
+++ packages/traceability/issuer-api/src/pods/options/types.ts
@@ -1,9 +1,7 @@
 export interface IssuerModuleOptions {
-    enableTransactionLogging: boolean;
     enableCertificationRequest: boolean;
 }
 
 export interface IssuerModuleInputOptions {
-    enableTransactionLogging?: boolean;
     enableCertificationRequest?: boolean;
 }
