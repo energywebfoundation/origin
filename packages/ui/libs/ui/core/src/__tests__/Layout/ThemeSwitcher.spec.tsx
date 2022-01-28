8683b7d55bc72c33e4dbce1657eb87cc248a7eeb
driamend <driamend@gmail.com>
2022-01-28 16:59:39 +0200

    chore(origin-ui-core): remove waitFor import

--- packages/ui/libs/ui/core/src/__tests__/Layout/ThemeSwitcher.spec.tsx
+++ packages/ui/libs/ui/core/src/__tests__/Layout/ThemeSwitcher.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { composeStories } from '@storybook/testing-react';
-import { render, fireEvent, act, waitFor } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import * as stories from '../../components/layout/ThemeSwitcher/ThemeSwitcher.stories';
