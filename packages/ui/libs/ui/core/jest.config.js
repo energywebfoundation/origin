855e6f4c70e10f216e1dbff9d38398d116d7091a
driamend <driamend@gmail.com>
2022-02-02 13:34:32 +0200

    chore(origin-ui-core): move mock folder to src

--- packages/ui/libs/ui/core/jest.config.js
+++ packages/ui/libs/ui/core/jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/assetsTransformer',
-    '\\.svg$': '<rootDir>/__mocks__/svg.js',
+    '\\.svg$': '<rootDir>/src/__mocks__/svg.js',
   },
 };
